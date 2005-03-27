df1b5e74d5808dab7edb02f091cb7eb18c3225ee
Paul M. Jones <pmjones@solarphp.com>
2005-03-27 04:01:06 +0000

    removed old .tpl.php versions

--- Solar/App/Bugs/views/error.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php include $this->template('header.tpl.php') ?>
-
-<div style="color: red;">
-<?php while ($err = $this->error->pop()): ?>
-	<p><?php echo $this->scrub($err['class::code'] . ' -- ' . $err['text']) ?></p>
-<?php endwhile; ?>
-</div>
-
-<?php include $this->template('footer.tpl.php') ?>

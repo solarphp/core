b2b8f7f65c8f87a4e0b14964be3e5e5537aa652d
Paul M. Jones <pmjones@solarphp.com>
2005-04-10 23:22:47 +0000

    support RSS feed requests

--- Solar/App/Bookmarks/controllers/tag.php
+++ Solar/App/Bookmarks/controllers/tag.php
@@ -20,14 +20,6 @@ $order = $this->getOrder();
 // what page-number of the results are we looking for?
 $page = Solar::get('page', 0);
 
-// RSS or HTML?
-$rss = Solar::get('rss', false);
-if ($rss) {
-	$tpl->setTemplate('rss.php');
-} else {
-	$tpl->setTemplate('list.php');
-}
-
 // get the list of results
 if (! $tags) {
 	// no tags requested
@@ -37,7 +29,16 @@ if (! $tags) {
 	$tpl->list = $bookmarks->withTags($tags, $user_id, $order, $page);
 };
 
+// RSS or HTML?
+$rss = Solar::get('rss', false);
+if ($rss) {
+	$tpl->setTemplate('rss.php');
+} else {
+	$tpl->setTemplate('list.php');
+}
+
 // assign everything else and display
+$tpl->rss['avail'] = true;
 $tpl->user_id = null; // requested user_id
 $tpl->tags = $tags; // requested tags
 $tpl->user_tags = null; // all tags for this user

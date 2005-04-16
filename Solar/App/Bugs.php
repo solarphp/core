43c0f22a8c3ebb470ba78b3607da7c21556f91b0
Paul M. Jones <pmjones@solarphp.com>
2005-04-16 18:58:15 +0000

    added setup method added config keys for admin_user and admin_role

--- Solar/App/Bugs.php
+++ Solar/App/Bugs.php
@@ -39,8 +39,18 @@ require_once 'Solar/App.php';
 */
 
 class Solar_App_Bugs extends Solar_App {
-	protected $action_src = 'get';
-	protected $action_var = 'action';
-	protected $action_default = 'listOpen';
+	protected function setup()
+	{
+		// controller action source and default
+		$this->action_src = 'get';
+		$this->action_var = 'action';
+		$this->action_default = 'listOpen';
+		
+		// the list of users who are allowed admin privs
+		$this->config['admin_user'] = array();
+		
+		// the list of roles that are allowed admin privs
+		$this->config['admin_role'] = array();
+	}
 }
 ?>

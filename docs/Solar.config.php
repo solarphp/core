a886396bb8a249ae4a17c96bac7129409156320d
Paul M. Jones <pmjones@solarphp.com>
2005-04-23 19:14:34 +0000

    updated with new example configs

--- docs/Solar.config.php
+++ docs/Solar.config.php
@@ -16,6 +16,7 @@
 * 
 */
 
+
 /**
 * Empty base array.
 */
@@ -24,38 +25,20 @@ $conf = array();
 
 
 /**
+* Document root.
+*/
+
+$docroot = $_SERVER['DOCUMENT_ROOT'];
+
+
+/**
 * General ini settings.
 */
 
 $config['Solar']['ini_set'] = array(
 	'error_reporting' => (E_ALL|E_STRICT),
-	'display_errors' => true,
-	'html_errors' => true
-);
-
-
-/**
-* Shared objects to load as called.
-*/
-
-$config['Solar']['shared'] = array(
-	'cache' => 'Solar_Cache', // uses default config
-	'sql'   => 'Solar_Sql', // uses default config
-	'user'  => 'Solar_User' // uses default config
-	/*
-	'My_Sql_Object'   => array( // uses custom config
-		'Solar_Sql',
-		array(...) // these are the custom configs
-	),
-	*/
-);
-
-/**
-* Always load these shared objects.
-*/
-$config['Solar']['autoshare'] = array(
-	'sql',
-	'user'
+	'display_errors'  => true,
+	'html_errors'     => true
 );
 
 
@@ -64,48 +47,20 @@ $config['Solar']['autoshare'] = array(
 */
 
 $config['Solar_Sql'] = array(
-	'class' => 'Solar_Sql_Driver_Mysql',
-	'host'  => 'localhost',
-	'user'  => 'somebody',
-	'pass'  => '********',
-	'name'  => 'database'
+	'class' => 'Solar_Sql_Driver_Sqlite',
+	'file'  => "$docroot/solar.sqlite",
+	'more'  => '0666',
 );
 
 
 /**
-* Full user config, with authentication source and group/role capture.
+* User authentication source.
 */
 
-$config['Solar_User'] = array(
-	'auth' => array(
-		'class' => 'Solar_User_Auth_Ldap',
-		'options' => array(
-			'url' => 'ldap://ds4.memphis.edu/',
-			'format' => 'uid=%s,ou=people,o=the university of memphis,st=tn,c=us'
-		),
-	),
-	'role' => array(
-		'refresh' => false,
-		'class' => 'Solar_User_Role_Multi',
-		'options' => array(
-			0 => array(
-				'Solar_User_Role_Ldap',
-				array(
-					'url'    => 'ldap://ds4.memphis.edu/',
-					'basedn' => 'ou=people,o=the university of memphis,st=tn,c=us',
-					'filter' => 'uid=%s',
-					'attrib' => array('ou', 'umemphisaffiliation'),
-					'binddn' => 'uid=svcdn-myclass, ou=Special Users, o=The University of Memphis, st=TN, c=us',
-					'bindpw' => '********'
-				),
-			),
-			1 => array(
-				'Solar_User_Role_File',
-				array(
-					'file' => '/var/www/conf/groups.txt'
-				),
-			),
-		),
+$config['Solar_User_Auth'] = array(
+	'class'    => 'Solar_User_Auth_Htpasswd',
+	'options'  => array(
+		'file' => "$docroot/htpasswd.conf",
 	),
 );
 

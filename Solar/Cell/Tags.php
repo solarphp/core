55372864c4374868c4e7c0ed1a1e53bc24e19fd2
Paul M. Jones <pmjones@solarphp.com>
2005-05-24 22:41:33 +0000

    * Solar_Cell_Tags now uses modified (and more efficient) search algorithm for tag lookups to related tables; the new methods are called relatedFetch() and relatedCountPages().
    * Solar_Cell_Tags no longer uses the "bundle" entity; you
    can issue "DROP TABLE sc_tags_bundles" to drop the
    unnecessary table.

--- Solar/Cell/Tags.php
+++ Solar/Cell/Tags.php
@@ -49,12 +49,9 @@ class Solar_Cell_Tags extends Solar_Sql_Entity {
 	);
 	
 	
-	public $bundle;
-	
 	public function __construct($config = null)
 	{
 		parent::__construct($config);
-		$this->bundle = Solar::object('Solar_Cell_Tags_Bundle');
 	}
 	
 	/**
@@ -139,6 +136,10 @@ class Solar_Cell_Tags extends Solar_Sql_Entity {
 			'fetch'  => 'Col'
 		);
 		
+		// placeholders for the relatedFetch() method.  will
+		// redefine these on-the-fly.
+		$schema['qry']['lookup'] = array();
+		$schema['rel']['lookup'] = '';
 		
 		// -------------------------------------------------------------
 		// 
@@ -221,27 +222,6 @@ class Solar_Cell_Tags extends Solar_Sql_Entity {
 			}
 		}
 		
-		// now refresh the bundle (for searches) ... but only
-		// if the tags changed.
-		if ($diff['del'] || $diff['ins']) {
-			
-			// delete previous
-			$this->bundle->remove($rel, $rel_id);
-			
-			// build data
-			$data = array(
-				'rel'    => $rel,
-				'rel_id' => $rel_id,
-				'tags'   => $tags
-			);
-			
-			$result = $this->bundle->insert($data);
-			
-			if (Solar::isError($result)) {
-				$err->push($result);
-			}
-		}
-		
 		// done!
 		if ($err->count() > 0) {
 			return $err;
@@ -274,6 +254,78 @@ class Solar_Cell_Tags extends Solar_Sql_Entity {
 	
 	/**
 	* 
+	* Does a lookup on the related table returning specified columns.
+	* 
+	* @todo Build a base 'lookup' query and then copy/edit it for
+	* customization, similar to countPages() in Entity.
+	* 
+	*/
+	
+	public function relatedFetch($rel, $tags, $cols = '*', $where = null,
+		$having = null, $order = null, $page = null)
+	{
+		// redefine the lookup relation on the fly so that it points to 
+		// the requested related table.
+		$this->schema['rel']['lookup'] = array(
+			'table' => 'sc_tags',
+			'on'    => array(
+				'rel'    => $this->quote($rel),
+				'rel_id' => "$rel.id",
+			),
+		);
+		
+		// redefine the lookup query on the fly
+		$qry =& $this->schema['qry']['lookup'];
+		
+		$qry['select'] = $cols;
+		$qry['from'] = $rel;
+		$qry['join'] = 'lookup';
+	
+		// add the tag WHERE.  convert $tags to array...
+		if (! is_array($tags)) {
+			$tags = $this->fixString($tags);
+			$tags = explode(' ', $tags);
+		}
+		
+		// ... get the tags as part of an IN() list ...
+		$taglist = array();
+		foreach ($tags as $tag) {
+			if (trim($tag) != '') {
+				// quote and add to the list of tags
+				$taglist[] = $this->quote($tag);
+			}
+		}
+		
+		// and build the WHERE clause.
+		$qry['where'] = 'sc_tags.tag IN (' . implode(', ', $taglist) . ')';
+		
+		// add the grouping; this lets us count the result hits by single
+		// related IDs.
+		$qry['group'] = 'sc_tags.rel_id';
+		
+		// add the HAVING clause that counts the tag return;
+		// this is what lets us avoid LIKE-AND queries.
+		$qry['having'] = " COUNT(sc_tags.rel_id) = " . count($taglist);
+		
+		// always fetch all results
+		$qry['fetch'] = 'All';
+		
+		// count only related IDs
+		$qry['count'] = "sc_tags.rel_id";
+		
+		// done redefining the query. return the fetch results.
+		return $this->selectFetch('lookup', $where, $having, $order, $page);
+	}
+	
+	// dumb and lazy -- only works for the last relatedFetch()
+	public function relatedCountPages($where = null, $having = null)
+	{
+		return $this->countPages('lookup', $where, $having);
+	}
+	
+	
+	/**
+	* 
 	* Fixes tag strings for the queries.
 	* 
 	*/
@@ -283,7 +335,7 @@ class Solar_Cell_Tags extends Solar_Sql_Entity {
 		// convert to array from string?
 		if (! is_array($tags)) {
 			
-			// convert all "+" to spaces
+			// convert all "+" to spaces (this is for URL values)
 			$tags = str_replace('+', ' ', $tags);
 			
 			// trim all surrounding spaces and extra spaces
@@ -297,7 +349,7 @@ class Solar_Cell_Tags extends Solar_Sql_Entity {
 		// make sure each tag is unique (no double-entries)
 		$tmp = array_unique($tmp);
 		
-		// return as text
+		// return as space-separated text
 		return implode(' ', $tmp);
 	}
 }

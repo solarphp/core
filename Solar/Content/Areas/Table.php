f45635e98603a69575caeb72825c6b0372653bcc
Paul M. Jones <pmjones@solarphp.com>
2005-10-06 01:22:15 +0000

    preparing to remove

--- Solar/Content/Areas/Table.php
+++ Solar/Content/Areas/Table.php
@@ -33,52 +33,5 @@
 class Solar_Content_Areas_Table extends Solar_Sql_Table {
 	
 	
-	/**
-	* 
-	* Schema setup.
-	* 
-	* @access protected
-	* 
-	* @return void
-	* 
-	*/
-	
-	protected function setup()
-	{
-		// the table name
-		$this->name = 'areas';
-		
-		// the area name
-		$this->col['name'] = array(
-			'type'    => 'varchar',
-			'size'    => 127,
-			'require' => true,
-			'valid'   => 'word',
-		);
-		
-		// the user who owns this area
-		$this->col['owner_handle'] = array(
-			'type'    => 'varchar',
-			'size'    => 32,
-		);
-		
-		// freeform area "subject" or title
-		$this->col['subj'] = array(
-			'type'    => 'varchar',
-			'size'    => 255,
-		);
-		
-		// serialized preferences
-		$this->col['prefs'] = array(
-			'type'    => 'clob',
-		);
-		
-		
-		// keys and indexes
-		$this->idx = array(
-			'name'         => 'unique',
-			'owner_handle' => 'normal',
-		);
-	}
 }
 ?>

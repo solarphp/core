a20fb6cb2b63e9be03d77c7e5ba3a557166107aa
Paul M. Jones <pmjones@solarphp.com>
2005-10-09 19:00:54 +0000

    added Bookmarks cell based on new content storage model
    A    Cell/Bookmarks
    A    Cell/Bookmarks/Locale
    AM   Cell/Bookmarks/Locale/en_US.php
    AM   Cell/Bookmarks.php

---
+++ Solar/Cell/Bookmarks/Locale/en_US.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+* 
+* Locale file.  Returns the strings for a specific language.
+* 
+* @category Solar
+* 
+* @package Solar
+* 
+* @subpackage Solar_Cell
+* 
+* @author Paul M. Jones <pmjones@solarphp.com>
+* 
+* @license LGPL
+* 
+* @version $Id$
+* 
+*/
+
+return array(
+	// labels and descriptions of node columns
+	'LABEL_URI'  => 'Location',
+	'LABEL_SUBJ' => 'Title',
+	'LABEL_SUMM' => 'Description',
+	'LABEL_TAGS' => 'Tags',
+	'LABEL_RANK' => 'Rank',
+);
+?>

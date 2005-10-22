b839b11f98b67109cbfce9a410a66b7597df1256
Paul M. Jones <pmjones@solarphp.com>
2005-10-22 16:56:38 +0000

    added comments
    M    Result.php

--- Solar/Sql/Result.php
+++ Solar/Sql/Result.php
@@ -20,7 +20,7 @@
 
 /**
 * 
-* Class for iterating through selected row results.
+* Class for fetching selected row results.
 * 
 * @category Solar
 * 
@@ -32,46 +32,97 @@
 
 class Solar_Sql_Result extends Solar_Base {
 	
+	
+	/**
+	* 
+	* User-defined configuration keys.
+	* 
+	* Keys are:
+	* 
+	* PDOStatment: (object) A PDOStatement object to be used as the
+	* result source.
+	* 
+	* @access protected
+	* 
+	* @var array
+	*/
+	
 	protected $config = array(
 		'PDOStatement' => null,
 	);
 	
-	/*
-	public function __call($func, $args)
-	{
-		return call_user_func_array(
-			array($this->config['PDOStatement'], $func),
-			$args
-		);
-	}
+	
+	/**
+	* 
+	* Fetches one row from the result source.
+	* 
+	* If a row name has double-underscores, the result is placed into a
+	* sub-array named for the part before the double-underscores.  For
+	* example, if the row-name is "example__row_name", then you would
+	* get back example['row_name'].
+	* 
+	* When combined with the automated deconfliction in
+	* Solar_Sql_Select, this allows you to select from multiple tables
+	* and segregate the columns from different tables automatically into
+	* separate arrays.
+	* 
+	* @access public
+	* 
+	* @param int $mode A PDO_FETCH_* constant to specify how the row
+	* should be returned; default is PDO_FETCH_ASSOC.
+	* 
+	* @return array An array of data from the fetched row.
+	* 
+	* @todo In colname-to-array deconfliction: what if a natural colname
+	* is the same as a table name? Then the one will overwrite the
+	* other, which is bad juju.  Perhaps a separate array for table-based,
+	* and another for non-table-based, and merge at the end?
+	* 
 	*/
 	
 	public function fetch($mode = PDO_FETCH_ASSOC)
 	{
-		// the fetched row data 
+		// the fetched row data to be returned
 		$row = array();
-		$orig = $this->config['PDOStatement']->fetch($mode);
 		
+		// the data as originally returned by PDOStatement
+		$orig = $this->config['PDOStatement']->fetch($mode);
 		if (! $orig) {
 			return false;
 		}
 		
-		// if the name has __ in it, assume that the
-		// left portion is the table name, and the
-		// right portion is the column name. otherwise
-		// it's just a column name.
+		// loop through each column of original data and merge into the
+		// $row array.
 		foreach ($orig as $key => $val) {
+			// does the column name have double-underscores in it?
 			$pos = strpos($key, '__');
 			if ($pos) {
+				// assume that the left portion is the table name, and
+				// the right portion is the column name.
 				$tbl = substr($key, 0, $pos);
 				$col = substr($key, $pos+2);
 				$row[$tbl][$col] = $val;
 			} else {
+				// no underscores, it's just a column name.
 				$row[$key] = $val;
 			}
 		}
 		return $row;
 	}
+	
+	
+	/**
+	* 
+	* Fetches all rows from the result source via $this->fetch().
+	* 
+	* @access public
+	* 
+	* @param int $mode A PDO_FETCH_* constant to specify how the row
+	* should be returned; default is PDO_FETCH_ASSOC.
+	* 
+	* @return array A sequential array of data from all fetched rows.
+	* 
+	*/
 	
 	public function fetchAll($mode = PDO_FETCH_ASSOC)
 	{

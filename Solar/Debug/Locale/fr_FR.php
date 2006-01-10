47ab4de8005df7a5e14aa6013d6e62b9db63da40
Paul M. Jones <pmjones@solarphp.com>
2006-01-10 19:52:16 +0000

    added keyword subs

--- Solar/Debug/Locale/fr_FR.php
+++ Solar/Debug/Locale/fr_FR.php
@@ -1,27 +1,27 @@
 <?php
 
 /**
-* 
-* Locale file.  Returns the strings for a specific language.
-* 
-* @category Solar
-* 
-* @package Solar
-* 
-* @subpackage Solar_Debug
-* 
-* @author Jean-Eric Laurent <jel@jelaurent.com>
-* 
-* @license LGPL
-* 
-* @version $Id: en_US.php 387 2005-07-04 19:33:44Z pmjones $
-* 
-*/
+ * 
+ * Locale file.  Returns the strings for a specific language.
+ * 
+ * @category Solar
+ * 
+ * @package Solar
+ * 
+ * @subpackage Solar_Debug
+ * 
+ * @author Jean-Eric Laurent <jel@jelaurent.com>
+ * 
+ * @license LGPL
+ * 
+ * @version $Id$
+ * 
+ */
 
 return array(
-	'LABEL_NAME'  => 'nom',
-	'LABEL_TIME'  => 'heure',
-	'LABEL_DIFF'  => 'diff',
-	'LABEL_TOTAL' => 'total',
+    'LABEL_NAME'  => 'nom',
+    'LABEL_TIME'  => 'heure',
+    'LABEL_DIFF'  => 'diff',
+    'LABEL_TOTAL' => 'total',
 );
 ?>

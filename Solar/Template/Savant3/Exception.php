a29952173e164e2a6ee2063e7026868142792cae
Paul M. Jones <pmjones@solarphp.com>
2006-01-10 20:41:17 +0000

    changed "public static" et al to "static public"

--- Solar/Template/Savant3/Exception.php
+++ Solar/Template/Savant3/Exception.php
@@ -1,28 +1,28 @@
 <?php
 /**
-* 
-* Throws PHP5 exceptions for Savant.
-*
-* @package Savant3
-* 
-* @author Paul M. Jones <pmjones@ciaweb.net>
-* 
-* @license http://www.gnu.org/copyleft/lesser.html LGPL
-* 
-* @version $Id$
-* 
-*/
+ * 
+ * Throws PHP5 exceptions for Savant.
+ *
+ * @package Savant3
+ * 
+ * @author Paul M. Jones <pmjones@ciaweb.net>
+ * 
+ * @license http://www.gnu.org/copyleft/lesser.html LGPL
+ * 
+ * @version $Id$
+ * 
+ */
 
 
 /**
-* 
-* A simple Savant3_Exception class.
-* 
-* @package Savant3
-* 
-* @author Paul M. Jones <pmjones@ciaweb.net>
-* 
-*/
+ * 
+ * A simple Savant3_Exception class.
+ * 
+ * @package Savant3
+ * 
+ * @author Paul M. Jones <pmjones@ciaweb.net>
+ * 
+ */
 
 class Savant3_Exception extends Exception {
 }
--- Solar/Template/Savant3/resources/Savant3_Plugin_htmlAttribs.php
+++ Solar/Template/Savant3/resources/Savant3_Plugin_htmlAttribs.php
@@ -1,56 +1,56 @@
 <?php
 
 /**
-* 
-* Plugin to convert an associative array to a string of tag attributes.
-* 
-* @package Savant3
-* 
-* @author Paul M. Jones <pmjones@ciaweb.net>
-* 
-* @license http://www.gnu.org/copyleft/lesser.html LGPL
-* 
-* @version $Id$
-* 
-*/
+ * 
+ * Plugin to convert an associative array to a string of tag attributes.
+ * 
+ * @package Savant3
+ * 
+ * @author Paul M. Jones <pmjones@ciaweb.net>
+ * 
+ * @license http://www.gnu.org/copyleft/lesser.html LGPL
+ * 
+ * @version $Id$
+ * 
+ */
 
 /**
-* 
-* Plugin to convert an associative array to a string of tag attributes.
-* 
-* @package Savant3
-* 
-* @author Paul M. Jones <pmjones@ciaweb.net>
-* 
-*/
+ * 
+ * Plugin to convert an associative array to a string of tag attributes.
+ * 
+ * @package Savant3
+ * 
+ * @author Paul M. Jones <pmjones@ciaweb.net>
+ * 
+ */
 
 class Savant3_Plugin_htmlAttribs extends Savant3_Plugin {
 
-	/**
-	* 
-	* Converts an associative array to a string of tag attributes.
-	* 
-	* @access public
-	* 
-	* @param array $attribs From this array, each key-value pair is 
-	* converted to an attribute name and value.
-	* 
-	* @return string The XHTML for the attributes.
-	* 
-	*/
-	
-	public function htmlAttribs($attribs)
-	{
-		$xhtml = '';
-		foreach ((array) $attribs as $key => $val) {
-			$key = htmlspecialchars($key);
-			if (is_array($val)) {
-				$val = implode(' ', $val);
-			}
-			$val = htmlspecialchars($val);
-			$xhtml .= " $key=\"$val\"";
-		}
-		return $xhtml;
-	}
+    /**
+     * 
+     * Converts an associative array to a string of tag attributes.
+     * 
+     * @access public
+     * 
+     * @param array $attribs From this array, each key-value pair is 
+     * converted to an attribute name and value.
+     * 
+     * @return string The XHTML for the attributes.
+     * 
+     */
+    
+    public function htmlAttribs($attribs)
+    {
+        $xhtml = '';
+        foreach ((array) $attribs as $key => $val) {
+            $key = htmlspecialchars($key);
+            if (is_array($val)) {
+                $val = implode(' ', $val);
+            }
+            $val = htmlspecialchars($val);
+            $xhtml .= " $key=\"$val\"";
+        }
+        return $xhtml;
+    }
 }
 ?>

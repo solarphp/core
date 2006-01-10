796dc329757d28827ddc494f7040a7121f3038e0
Paul M. Jones <pmjones@solarphp.com>
2006-01-10 19:45:27 +0000

    added id keyword subs
    M    en_US.php

--- Solar/Cell/Comments/Locale/en_US.php
+++ Solar/Cell/Comments/Locale/en_US.php
@@ -1,28 +1,28 @@
 <?php
 
 /**
-* 
-* Locale file.  Returns the strings for a specific language.
-* 
-* @category Solar
-* 
-* @package Solar_Cell
-* 
-* @subpackage Solar_Cell_Comments
-* 
-* @author Paul M. Jones <pmjones@solarphp.com>
-* 
-* @license LGPL
-* 
-* @version $Id: en_US.php 567 2005-10-09 19:00:54Z pmjones $
-* 
-*/
+ * 
+ * Locale file.  Returns the strings for a specific language.
+ * 
+ * @category Solar
+ * 
+ * @package Solar_Cell
+ * 
+ * @subpackage Solar_Cell_Comments
+ * 
+ * @author Paul M. Jones <pmjones@solarphp.com>
+ * 
+ * @license LGPL
+ * 
+ * @version $Id$
+ * 
+ */
 
 return array(
-	// labels and descriptions of node columns
-	'LABEL_SUMM'  => 'Your Name',
-	'LABEL_EMAIL' => 'Email',
-	'LABEL_URI'   => 'Website',
-	'LABEL_BODY'  => 'Comment',
+    // labels and descriptions of node columns
+    'LABEL_SUMM'  => 'Your Name',
+    'LABEL_EMAIL' => 'Email',
+    'LABEL_URI'   => 'Website',
+    'LABEL_BODY'  => 'Comment',
 );
 ?>

733e6dc94e5d9af5d7cdfc64bc0a51d55d607ab6
Paul M. Jones <pmjones@solarphp.com>
2006-01-31 01:26:25 +0000

    added generic Solar_Sql_Exception class

---
+++ Solar/Sql/Exception.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * 
+ * Exception class.
+ * 
+ * @category Solar
+ * 
+ * @package Solar
+ * 
+ * @author Paul M. Jones <pmjones@solarphp.com>
+ * 
+ * @license LGPL
+ * 
+ * @version $Id: Exception.php 656 2006-01-14 19:02:13Z pmjones $
+ * 
+ */
+
+/**
+ * 
+ * Exception class.
+ * 
+ * @category Solar
+ * 
+ * @package Solar
+ * 
+ */
+class Solar_Sql_Exception extends Solar_Exception {}
+?>

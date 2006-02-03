06c78d9ec68c53b572087f9444692b9fc7c787bf
Paul M. Jones <pmjones@solarphp.com>
2006-02-03 01:15:33 +0000

    * Internal errors are now Savant errors, not Solar errors (i.e., removed error() and isError() extension methods)
    M    Template.php

--- Solar/Template.php
+++ Solar/Template.php
@@ -79,45 +79,5 @@ class Solar_Template extends Savant3 {
         // ... and pass to the Savant3 constructor.
         parent::__construct($config);
     }
-    
-    /**
-     *
-     * Returns a Solar_Error object.
-     * 
-     * @access public
-     * 
-     * @param string $code A Savant3 'ERR_*' string.
-     * 
-     * @param array $info An array of error-specific information.
-     * 
-     * @return Solar_Error
-     * 
-     */
-    public function error($code, $info = array(), $level = E_USER_ERROR,
-        $trace = true)
-    {
-        $class = get_class($this);
-        $text = Solar::locale($class, $code); // will this work?
-        settype($info, 'array');
-        $level = E_USER_ERROR; // all errors are showstoppers
-        $err = Solar::error($class, $code, $text, $info, $level, $trace);
-        return $err;
-    }
-    
-    /**
-     *
-     * Tests if an object is an error.
-     * 
-     * @access public
-     * 
-     * @param object $obj The object to be tested.
-     * 
-     * @return boolean True if $obj is a Solar_Error.
-     *
-     */
-    public function isError($obj)
-    {
-        return Solar::isError($obj);
-    }
 }
 ?>

4be2112762c1d31b6297640fb1741bb8113d1513
Paul M. Jones <pmjones@solarphp.com>
2006-05-23 23:18:16 +0000

    per Mike N. and notes from Matt W-O'P, make the test-runner work from include-path, from trunk SVN checkout (also a tarball download), and from the pear-installed location.

--- tests/run.php
+++ tests/run.php
@@ -1,5 +1,26 @@
 <?php
-require_once 'Solar.php';
+// assume Solar is on the include_path.
+include_once 'Solar.php';
+
+if (! class_exists('Solar')) {
+    // assume Solar is at $target/tests/.
+    // this is good for SVN checkouts
+    // and the as-delivered tarball/pearball.
+    include_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'Solar.php';
+}
+
+if (! class_exists('Solar')) {
+    // assume Solar is at $target/tests/Solar/tests/.
+    // this is good for when we're already pear-installed.
+    include_once dirname(dirname(dirname(dirname(__FILE__)))) . DIRECTORY_SEPARATOR . 'Solar.php';
+}
+
+if (! class_exists('Solar')) {
+    // not in any of the known configurations.
+    throw Exception('Cannot find Solar.php.');
+}
+
+// proceed with testing
 $config = dirname(__FILE__) . '/config.inc.php';
 Solar::start($config);
 

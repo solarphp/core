386c65b6915a4eb18466cd4e0976c9f2561c19a1
Paul M. Jones <pmjones@solarphp.com>
2006-05-24 13:08:39 +0000

    uses the new Solar::$dir property to effectively set the include_path; this helps when running tests directly from a download or svn checkout

--- tests/run.php
+++ tests/run.php
@@ -1,18 +1,18 @@
 <?php
 // assume Solar is on the include_path.
-include_once 'Solar.php';
+@include_once 'Solar.php';
 
 if (! class_exists('Solar')) {
     // assume Solar is at $target/tests/.
     // this is good for SVN checkouts
     // and the as-delivered tarball/pearball.
-    include_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'Solar.php';
+    @include_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'Solar.php';
 }
 
 if (! class_exists('Solar')) {
     // assume Solar is at $target/tests/Solar/tests/.
     // this is good for when we're already pear-installed.
-    include_once dirname(dirname(dirname(dirname(__FILE__)))) . DIRECTORY_SEPARATOR . 'Solar.php';
+    @include_once dirname(dirname(dirname(dirname(__FILE__)))) . DIRECTORY_SEPARATOR . 'Solar.php';
 }
 
 if (! class_exists('Solar')) {
@@ -20,9 +20,15 @@ if (! class_exists('Solar')) {
     throw Exception('Cannot find Solar.php.');
 }
 
+// set the include-path to wherever Solar is
+
 // proceed with testing
 $config = dirname(__FILE__) . '/config.inc.php';
 Solar::start($config);
+
+// force the include_path to be the Solar directory
+$old_include_path = ini_get('include_path');
+ini_set('include_path', Solar::$dir);
 
 // configure and instantiate the suite
 $config = array(
@@ -35,5 +41,6 @@ $series = isset($argv[1]) ? trim($argv[1]) : null;
 $suite->run($series);
 
 // done
+ini_set('include_path', $old_include_path);
 Solar::stop();
 ?>

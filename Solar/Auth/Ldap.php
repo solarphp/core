c026b85dd5e4fa7f2840e8f87232bccc90ea1054
Paul M. Jones <pmjones@solarphp.com>
2006-05-26 00:55:44 +0000

    began move of Solar_User_Auth to Solar_Auth

---
+++ Solar/Auth/Ldap.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * 
+ * Authenticate against an LDAP server.
+ * 
+ * @category Solar
+ * 
+ * @package Solar_User
+ * 
+ * @author Paul M. Jones <pmjones@solarphp.com>
+ * 
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ * 
+ * @version $Id$
+ * 
+ */
+
+/**
+ * 
+ * Authenticate against an LDAP server.
+ * 
+ * @category Solar
+ * 
+ * @package Solar_User
+ * 
+ */
+class Solar_User_Auth_Ldap extends Solar_Base {
+    
+    /**
+     * 
+     * User-supplied configuration values.
+     * 
+     * Keys are:
+     * 
+     * : \\url\\ : (string) URL to the LDAP server, e.g. "ldaps://example.com:389".
+     * 
+     * : \\format\\ : (string) Sprintf() format string for the LDAP query; %s
+     * represents the username.  Example: "uid=%s,dc=example,dc=com".
+     * 
+     * @var array
+     * 
+     */
+    protected $_config = array(
+        'url'    => null,
+        'format' => null,
+    );
+    
+    /**
+     * 
+     * Constructor.
+     * 
+     * @param array $config User-defined configuration.
+     * 
+     */
+    public function __construct($config = null)
+    {
+        // make sure we have LDAP available
+        if (! extension_loaded('ldap')) {
+            throw $this->_exception(
+                'ERR_EXTENSION_NOT_LOADED',
+                array('extension' => 'ldap')
+            );
+        }
+        
+        // continue construction
+        parent::__construct($config);
+    }
+
+
+    /**
+     * 
+     * Validate a username and password.
+     * 
+     * @param string $handle Username to authenticate.
+     * 
+     * @param string $passwd The plain-text password to use.
+     * 
+     * @return bool True on success, false on failure.
+     * 
+     */
+    public function valid($handle, $passwd)
+    {
+        // connect
+        $conn = @ldap_connect($this->_config['url']);
+        
+        // did the connection work?
+        if (! $conn) {
+            throw $this->_exception(
+                'ERR_CONNECTION_FAILED',
+                array($this->_config)
+            );
+        }
+        
+        // upgrade to LDAP3 when possible
+        @ldap_set_option($conn, LDAP_OPT_PROTOCOL_VERSION, 3);
+        
+        // bind to the server
+        $rdn = sprintf($this->_config['format'], $handle);
+        $bind = @ldap_bind($conn, $rdn, $passwd);
+        ldap_close($conn);
+        
+        // return the bind-value
+        if (! $bind) {
+            // not using $this->_exception() because we need fine control
+            // over the error text
+            throw Solar::exception(
+                get_class($this),
+                @ldap_errno($conn),
+                @ldap_error($conn),
+                array($this->_config)
+            );
+        } else {
+            return true;
+        }
+    }
+}
+?>
---
+++ Solar/Auth/None.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * 
+ * Authenticate against nothing; defaults all authentication to "failed."
+ * 
+ * @category Solar
+ * 
+ * @package Solar_User
+ * 
+ * @author Paul M. Jones <pmjones@solarphp.com>
+ * 
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ * 
+ * @version $Id$
+ * 
+ */
+
+/**
+ * 
+ * Authenticate against nothing; defaults all authentication to "failed."
+ * 
+ * @category Solar
+ * 
+ * @package Solar_User
+ * 
+ */
+class Solar_User_Auth_None extends Solar_Base {
+    
+    /**
+     * 
+     * Validate a username and password.  Always fails.
+     * 
+     * @param string $handle Username to authenticate.
+     * 
+     * @param string $passwd The plain-text password to use.
+     * 
+     * @return bool True on success, false on failure.
+     * 
+     */
+    public function valid($handle, $passwd)
+    {
+        return false;
+    }
+}
+?>

70ec5c7bae94411bfe235188cdec8d559de11155
Paul M. Jones <pmjones@solarphp.com>
2006-06-22 19:14:29 +0000

    converted to action methods instead of action files

--- Solar/App/Hello.php
+++ Solar/App/Hello.php
@@ -92,5 +92,35 @@ class Solar_App_Hello extends Solar_App {
         // set the layout title
         $this->layout_title = get_class($this);
     }
+    
+    public function mainAction()
+    {
+        // get the locale code, default is en_US
+        $this->code = $this->_query('code', 'en_US');
+
+        // reset the locale strings to the new code
+        Solar::setLocale($this->code);
+
+        // set the translated text
+        $this->text = $this->locale('TEXT_HELLO_WORLD');
+
+        // tell the site layout what title to use
+        $this->layout_title = 'Solar: Hello World!';
+    }
+    
+    public function rssAction()
+    {
+        // get the locale code, default is en_US
+        $this->code = $this->_query('code', 'en_US');
+
+        // reset the locale strings to the new code
+        Solar::setLocale($this->code);
+
+        // set the translated text
+        $this->text = $this->locale('TEXT_HELLO_WORLD');
+
+        // turn off the site layout
+        $this->_layout = false;
+    }
 }
 ?>

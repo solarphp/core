690355beb6196c955ca1f6630665374d0e4bf041
Paul M. Jones <pmjones@solarphp.com>
2006-06-22 23:04:37 +0000

    moved back to 'Solar' as the default locale class

--- Solar/View/Helper/GetTextRaw.php
+++ Solar/View/Helper/GetTextRaw.php
@@ -43,7 +43,7 @@ class Solar_View_Helper_GetTextRaw extends Solar_View_Helper {
      * 
      */
     protected $_config = array(
-        'class' => 'Solar_Base',
+        'class' => 'Solar',
     );
     
     /**

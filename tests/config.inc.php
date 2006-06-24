24fc67107264a535e61f9a0c9e24b8b6650bd8be
Paul M. Jones <pmjones@solarphp.com>
2006-06-24 19:44:19 +0000

    no parens needed

--- tests/config.inc.php
+++ tests/config.inc.php
@@ -3,7 +3,7 @@ $config = array();
 
 $config['Test']['include_path'] = dirname(dirname(__FILE__));
 
-$config['Solar']['ini_set']['error_reporting'] = (E_ALL | E_STRICT);
+$config['Solar']['ini_set']['error_reporting'] = E_ALL | E_STRICT;
 $config['Solar']['ini_set']['display_errors'] = true;
 $config['Solar']['ini_set']['date.timezone'] = 'America/Chicago';
 

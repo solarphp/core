09bbb756c0e28016481d4a725380e4c4bd12bbd2
Paul M. Jones <pmjones@solarphp.com>
2006-07-04 17:52:30 +0000

    updated existing tests added new tests for Sql auth adapter added initial test for Memcache cache adapter (fails)

--- tests/Test/Solar/Auth/Adapter/Ini.php
+++ tests/Test/Solar/Auth/Adapter/Ini.php
@@ -6,8 +6,8 @@ class Test_Solar_Auth_Adapter_Ini extends Test_Solar_Auth_Adapter {
     
     public function setup()
     {
-        $this->_config['file'] = dirname(dirname(__FILE__)) . '/users.ini';
-        $this->_name = 'Paul M. Jones';
+        $this->_config['file'] = dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'users.ini';
+        $this->_moniker = 'Paul M. Jones';
         $this->_email = 'pmjones@solarphp.com';
         $this->_uri = 'http://paul-m-jones.com';
         parent::setup();

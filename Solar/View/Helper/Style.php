e8fbc8b019036888152af84fcdba5aa8bb5a62f9
Clay Loveless <clay@claylo.com>
2006-08-22 18:39:57 +0000

    Allow remotely hosted stylesheets in Style helper

--- Solar/View/Helper/Style.php
+++ Solar/View/Helper/Style.php
@@ -1,49 +1,49 @@
 <?php
 /**
- * 
+ *
  * Helper for <style>...</style> tags.
- * 
+ *
  * @category Solar
- * 
+ *
  * @package Solar_View
- * 
+ *
  * @author Paul M. Jones <pmjones@solarphp.com>
- * 
+ *
  * @license http://opensource.org/licenses/bsd-license.php BSD
- * 
+ *
  * @version $Id$
- * 
+ *
  */
 
 /**
  * Solar_View_Helper
  */
 Solar::loadClass('Solar_View_Helper');
- 
+
 /**
- * 
+ *
  * Helper for <style>...</style> tags.
- * 
+ *
  * @category Solar
- * 
+ *
  * @package Solar_View
- * 
+ *
  */
 class Solar_View_Helper_Style extends Solar_View_Helper {
-    
+
     /**
-     * 
+     *
      * Returns a <style>...</style> tag.
-     * 
+     *
      * Adds "media" attribute if not specified, and always uses
      * "type" attribute of "text/css".
-     * 
+     *
      * @param string $href The source href for the stylesheet.
-     * 
+     *
      * @param array $attribs Additional attributes for the <style> tag.
-     * 
+     *
      * @return string The <style>...</style> tag.
-     * 
+     *
      */
     public function style($href, $attribs = null)
     {
@@ -52,7 +52,12 @@ class Solar_View_Helper_Style extends Solar_View_Helper {
         if (empty($attribs['media'])) {
             $attribs['media'] = 'screen';
         }
-        $url = $this->_view->publicHref($href, true);
+        if (substr($href, 0, 4) == 'http'
+            && strpos($href, '://') !== false) {
+            $url = $href;
+        } else {
+            $url = $this->_view->publicHref($href, true);
+        }
         return '<style' . $this->_view->attribs($attribs) . '>'
              . "@import url(\"$url\");</style>";
     }
--- tests/Test/Solar/View/Helper/Style.php
+++ tests/Test/Solar/View/Helper/Style.php
@@ -3,7 +3,7 @@
 require_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . '/Helper.php';
 
 class Test_Solar_View_Helper_Style extends Test_Solar_View_Helper {
-    
+
     public function testStyle()
     {
         $actual = $this->_view->style('styles.css');
@@ -11,5 +11,18 @@ class Test_Solar_View_Helper_Style extends Test_Solar_View_Helper {
                 . '@import url("/public/styles.css");</style>';
         $this->assertSame($actual, $expect);
     }
+
+    public function testStyle_Remote()
+    {
+        $actual = $this->_view->style('styles.css');
+        $actual .= $this->_view->style('http://example.com/styles.css');
+
+        $expect = '<style type="text/css" media="screen">'
+                . '@import url("/public/styles.css");</style>';
+        $expect .= '<style type="text/css" media="screen">'
+                . '@import url("http://example.com/styles.css");</style>';
+        $this->assertSame($actual, $expect);
+    }
+
 }
 ?>

0645483d5150e353b85e3fe22a4b2baccb57192e
Paul M. Jones <pmjones@solarphp.com>
2006-11-10 22:42:41 +0000

    made cache tests use an abstract parent class for standardization across adapters

--- tests/Test/Solar/Cache/Adapter/Memcache.php
+++ tests/Test/Solar/Cache/Adapter/Memcache.php
@@ -1,176 +1,33 @@
 <?php
-class Test_Solar_Cache_Adapter_Memcache extends Solar_Test {
+require_once realpath(dirname(__FILE__) . '/../Adapter.php');
 
-    protected $_cache;
+class Test_Solar_Cache_Adapter_Memcache extends Test_Solar_Cache_Adapter {
 
     protected $_Test_Solar_Cache_Adapter_Memcache = array(
         'adapter' => 'Solar_Cache_Adapter_Memcache',
         'config'  => array(
-            'host' => '127.0.0.1',
+            'host' => 'localhost',
             'port' => 11211,
             'life' => 7, // 7 seconds
         ),
-        'run' => false,
     );
 
     public function __construct($config = null)
     {
-        parent::__construct($config);
-        
         if (! extension_loaded('memcache')) {
             $this->skip('memcache extension not loaded');
         }
-        
-        if (! $this->_config['run']) {
-            $this->skip("config key 'run' set to false");
-        }
+        parent::__construct($config);
     }
     
     public function setup()
     {
-        // create a Solar_Cache with the Solar_Cache_Memcache adapter
-        $this->_cache = Solar::factory('Solar_Cache', $this->_config);
-
-        // remove all previous entries
-        $this->_cache->deleteAll();
+        parent::setup();
         
         // for some reason, we need to wait a second before trying
         // to add/remove entries from the cache.  otherwise all the
         // tests fail.
         sleep(1);
-    }
-    
-    public function testDelete()
-    {
-        $id = 'spiff';
-        $data = 'Spaceman Spiff';
-
-        // data has not been stored yet
-        $this->assertFalse($this->_cache->fetch($id));
-
-        // store it
-        $this->assertTrue($this->_cache->save($id, $data));
-
-        // and we should be able to fetch now
-        $this->assertSame($this->_cache->fetch($id), $data);
-
-        // delete it, should not be able to fetch again
-        $this->_cache->delete($id);
-        $this->assertFalse($this->_cache->fetch($id));
-    }
-
-    public function testDeleteAll()
-    {
-        $list = array(1, 2, 'five');
-        $data = 'Spaceman Spiff';
-
-        foreach ($list as $id) {
-            // data has not been stored yet
-            $this->assertFalse($this->_cache->fetch($id));
-            // so store some data
-            $this->assertTrue($this->_cache->save($id, $data));
-            // and we should be able to fetch now
-            $this->assertSame($this->_cache->fetch($id), $data);
-        }
-
-        // delete everything
-        $this->_cache->deleteAll();
-
-        // should not be able to fetch again
-        foreach ($list as $id) {
-            $this->assertFalse($this->_cache->fetch($id));
-        }
-    }
-
-    public function testFetch()
-    {
-        $id = 'spiff';
-        $data = 'Spaceman Spiff';
-
-        // data has not been stored yet
-        $this->assertFalse($this->_cache->fetch($id));
-
-        // store it
-        $this->assertTrue($this->_cache->save($id, $data));
-
-        // and we should be able to fetch now
-        $this->assertSame($this->_cache->fetch($id), $data);
-
-        // deactivate then try to fetch
-        $this->_cache->setActive(false);
-        $this->assertFalse($this->_cache->isActive());
-        $this->assertFalse($this->_cache->fetch($id));
-
-        // re-activate then try to fetch
-        $this->_cache->setActive(true);
-        $this->assertTrue($this->_cache->isActive());
-        $this->assertSame($this->_cache->fetch($id), $data);
-    }
-
-    public function testGetLife()
-    {
-        $id = 'spiff';
-        $data = 'Spaceman Spiff';
-
-        // configured from setup
-        $this->assertSame($this->_cache->getLife(), $this->_config['config']['life']);
-
-        // store something
-        $this->assertTrue($this->_cache->save($id, $data));
-        $this->assertSame($this->_cache->fetch($id), $data);
-
-        // wait until just before the lifetime,
-        // we should still get data
-        sleep($this->_cache->getLife() - 1);
-        $this->assertSame($this->_cache->fetch($id), $data);
-
-        // wait until just after the lifetime,
-        // we should get nothing
-        sleep(2);
-        $this->assertFalse($this->_cache->fetch($id));
-    }
-
-    public function testIsActive()
-    {
-        // should be active by default
-        $this->assertTrue($this->_cache->isActive());
-
-        // turn it off
-        $this->_cache->setActive(false);
-        $this->assertFalse($this->_cache->isActive());
-
-        // turn it back on
-        $this->_cache->setActive(true);
-        $this->assertTrue($this->_cache->isActive());
-    }
-
-    public function testSave_Array()
-    {
-        $id = 'spiff';
-        $data = array(
-            'name'   => 'Spiff',
-            'type'   => 'Spaceman',
-            'visits' => 'Zartron-9',
-            'action' => 'Reboot saucer\'s computer and recalibrate weapons',
-        );
-        $this->assertTrue($this->_cache->save($id, $data));
-        $this->assertSame($this->_cache->fetch($id), $data);
-    }
-
-    public function testSave_Object()
-    {
-        $id = 'spiff';
-        $data = Solar::factory('Solar_Test_Example');
-        $this->assertTrue($this->_cache->save($id, $data));
-        $this->assertEquals($this->_cache->fetch($id), $data);
-    }
-
-    public function testSave_String()
-    {
-        $id = 'spiff';
-        $data = 'Spaceman Spiff';
-        $this->assertTrue($this->_cache->save($id, $data));
-        $this->assertSame($this->_cache->fetch($id), $data);
     }
 }
 ?>
--- tests/Test/Solar/Cache/Adapter/Xcache.php
+++ tests/Test/Solar/Cache/Adapter/Xcache.php
@@ -1,7 +1,7 @@
 <?php
-class Test_Solar_Cache_Adapter_Xcache extends Solar_Test {
+require_once realpath(dirname(__FILE__) . '/../Adapter.php');
 
-    protected $_cache;
+class Test_Solar_Cache_Adapter_Xcache extends Test_Solar_Cache_Adapter {
 
     protected $_Test_Solar_Cache_Adapter_Xcache = array(
         'adapter' => 'Solar_Cache_Adapter_Xcache',
@@ -14,138 +14,10 @@ class Test_Solar_Cache_Adapter_Xcache extends Solar_Test {
 
     public function __construct($config = null)
     {
-        parent::__construct($config);
         if (! extension_loaded('xcache')) {
             $this->skip('xcache extension not loaded');
         }
-    }
-
-    public function setup()
-    {
-        // create a Solar_Cache with the Solar_Cache_Xcache adapter
-        $this->_cache = Solar::factory('Solar_Cache', $this->_config);
-
-        // remove all previous entries
-        $this->_cache->deleteAll();
-    }
-
-    public function tearDown()
-    {
-        // remove all previous entries
-        $this->_cache->deleteAll();
-    }
-
-    public function testDelete()
-    {
-        $id = 'spiff';
-        $data = 'Spaceman Spiff';
-
-        // data has not been stored yet
-        $this->assertNull($this->_cache->fetch($id));
-
-        // store it
-        $this->assertTrue($this->_cache->save($id, $data));
-
-        // and we should be able to fetch now
-        $this->assertSame($this->_cache->fetch($id), $data);
-
-        // delete it, should not be able to fetch again
-        $this->_cache->delete($id);
-        $this->assertNull($this->_cache->fetch($id));
-    }
-
-    public function testDeleteAll()
-    {
-        $list = array(1, 2, 'five');
-        $data = 'Spaceman Spiff';
-
-        foreach ($list as $id) {
-            // data has not been stored yet
-            $this->assertNull($this->_cache->fetch($id));
-            // so store some data
-            $this->assertTrue($this->_cache->save($id, $data));
-            // and we should be able to fetch now
-            $this->assertSame($this->_cache->fetch($id), $data);
-        }
-
-        // delete everything
-        $this->_cache->deleteAll();
-
-        // should not be able to fetch again
-        foreach ($list as $id) {
-            $this->assertNull($this->_cache->fetch($id));
-        }
-    }
-
-    public function testFetch()
-    {
-        $id = 'spiff';
-        $data = 'Spaceman Spiff';
-
-        // data has not been stored yet
-        $this->assertNull($this->_cache->fetch($id));
-
-        // store it
-        $this->assertTrue($this->_cache->save($id, $data));
-
-        // and we should be able to fetch now
-        $this->assertSame($this->_cache->fetch($id), $data);
-
-        // xcache has no ability to de-activate a cached value
-
-    }
-
-    public function testTTL()
-    {
-        $id = 'spiff';
-        $data = 'Spaceman Spiff';
-
-        // configured from setup
-        // xcache has no getLife()-type method
-        //$this->assertSame($this->_cache->getLife(), $this->_config['config']['life']);
-
-        // store something
-        $this->assertTrue($this->_cache->save($id, $data, 3));
-        $this->assertSame($this->_cache->fetch($id), $data);
-
-        // wait until just before the lifetime,
-        // we should still get data
-        sleep(1);
-        $this->assertSame($this->_cache->fetch($id), $data);
-
-        // wait until just after the lifetime,
-        // we should get nothing
-        sleep(5);
-        $this->assertNull($this->_cache->fetch($id));
-    }
-
-    public function testSave_Array()
-    {
-        $id = 'spiff';
-        $data = array(
-            'name'   => 'Spiff',
-            'type'   => 'Spaceman',
-            'visits' => 'Zartron-9',
-            'action' => 'Reboot saucer\'s computer and recalibrate weapons',
-        );
-        $this->assertTrue($this->_cache->save($id, $data));
-        $this->assertSame($this->_cache->fetch($id), $data);
-    }
-
-    public function testSave_Object()
-    {
-        $id = 'spiff';
-        $data = Solar::factory('Solar_Test_Example');
-        $this->assertTrue($this->_cache->save($id, $data));
-        $this->assertEquals($this->_cache->fetch($id), $data);
-    }
-
-    public function testSave_String()
-    {
-        $id = 'spiff';
-        $data = 'Spaceman Spiff';
-        $this->assertTrue($this->_cache->save($id, $data));
-        $this->assertSame($this->_cache->fetch($id), $data);
+        parent::__construct($config);
     }
 }
 ?>

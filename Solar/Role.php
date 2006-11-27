75c530073db661b360082fb6b06e4695f8aa7845
Paul M. Jones <pmjones@solarphp.com>
2006-11-27 16:46:34 +0000

    Solar_Role: converted from "facade" pattern to "factory" pattern.
    Solar_Role: [BRK] Is now a factory class, not a facade, and returns adapter instances instead of an instance of itself.  Although I note this as a BC-break, you should be able to continue using it almost exactly the way you have; i.e., through Solar::factory().  The only difference should be that you get back an adapter instance instead of the facade instance.
    
    Solar_Role_Adapter: [BRK] Now contains all the methods and properties previously contained in the Facade wrapper.  I note this as a BC break, but if you have been using Solar::factory() all along, you should experience few if any problems on upgrade.

--- Solar/Role.php
+++ Solar/Role.php
@@ -52,178 +52,25 @@ class Solar_Role extends Solar_Base {
     
     /**
      * 
-     * The adapter instance.
-     * 
-     * @var array
+     * Factory method for returning adapters.
      * 
      */
-    protected $_adapter = array();
-    
-    /**
-     * 
-     * Have we attempted to load the list of roles yet?
-     * 
-     * @var bool
-     * 
-     */
-    protected $_loaded = false;
-    
-    /**
-     * 
-     * A class-segmented session-variable reference.
-     * 
-     * @var Solar_Session
-     * 
-     */
-    protected $_session;
-    
-    /**
-     * 
-     * A public reference to the session store.
-     * 
-     * @var array
-     * 
-     */
-    public $list;
-    
-    /**
-     * 
-     * Constructor to set up the storage adapter.
-     * 
-     * @param array $config User-provided configuration values.
-     * 
-     */
-    public function __construct($config = null)
+    public function factory()
     {
-        // basic config option settings
-        parent::__construct($config);
+        // bring in the config and get the adapter class.
+        $config = $this->_config;
+        $class = $config['adapter'];
+        unset($config['adapter']);
         
-        // get a session segment
-        $this->_session = Solar::factory(
-            'Solar_Session',
-            array('class' => get_class($this))
-        );
-        
-        // instantiate a adapter object
-        $this->_adapter = Solar::factory(
-            $this->_config['adapter'],
-            $this->_config['config']
-        );
-        
-        // make sure we have a session value and reference to it.
-        $this->list =& $this->_session->store;
-    }
-    
-    /**
-     * 
-     * Load the list of roles for the given user from the adapter.
-     * 
-     * @param string $handle The username to load roles for.
-     * 
-     * @return void
-     * 
-     */
-    public function load($handle)
-    {
-        // have we loaded roles for the first time yet? if so, and if
-        // we're not forcing refreshes, the we don't need to do
-        // anything, just return the list as it is right now.
-        if ($this->_loaded && ! $this->_config['refresh']) {
-            return $this->list;
+        // deprecated: support a 'config' key for the adapter configs.
+        // this was needed for facades, but is not needed for factories.
+        if (isset($config['config'])) {
+            $tmp = $config['config'];
+            unset($config['config']);
+            $config = array_merge($config, (array) $tmp);
         }
         
-        // reset the roles list
-        $this->reset();
-        
-        // fetch the role list
-        $result = $this->_adapter->fetch($handle);
-        if ($result) {
-            // merge the results into the common list
-            $this->list = array_merge(
-                $this->list,
-                (array) $result
-            );
-        }
-        
-        // OK, we've loaded what we can.
-        $this->_loaded = true;
-    }
-    
-    /**
-     * 
-     * Resets the role list to nothing.
-     * 
-     * @return void
-     * 
-     */
-    public function reset()
-    {
-        $this->_loaded = false;
-        $this->list = array();
-    }
-    
-    /**
-     * 
-     * Check to see if a user is in a role.
-     * 
-     * @param string $role The role to check.
-     * 
-     * @return bool True if the user is in the role, or false if not.
-     * 
-     */
-    public function is($role = null)
-    {
-        return in_array($role, $this->list);
-    }
-    
-    /**
-     * 
-     * Check to see if a user is in any of the listed roles.
-     * 
-     * @param string|array $roles The role(s) to check.
-     * 
-     * @return bool True if the user is in any of the listed roles (a
-     * logical 'or'), false if not.
-     * 
-     */
-    public function isAny($roles = array())
-    {
-        // loop through all of the roles, returning 'true' the first
-        // time we find a matching role.
-        foreach ((array) $roles as $role) {
-            if (in_array($role, $this->list)) {
-                return true;
-            }
-        }
-        
-        // we got through the whole array without finding a match.
-        // therefore, user was not in any of the roles.
-        return false;
-    }
-    
-    /**
-     * 
-     * Check to see if a user is in all of the listed roles.
-     * 
-     * @param string|array $roles The role(s) to check.
-     * 
-     * @return bool True if the user is in all of the listed roles (a
-     * logical 'and'), false if not.
-     * 
-     */
-    public function isAll($roles = array())
-    {
-        // loop through all of the roles, returning 'false' the first
-        // time we find the user is not in one of the roles.
-        foreach ((array) $roles as $role) {
-            if (! in_array($role, $this->list)) {
-                return false;
-            }
-        }
-        
-        // we got through the whole list; therefore, the user is in all
-        // of the noted roles.
-        return true;
+        return Solar::factory($class, $config);
     }
 }
 ?>

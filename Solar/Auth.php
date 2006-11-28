af3710683dc603411dfe0d5b8983a3e9daffa468
Paul M. Jones <pmjones@solarphp.com>
2006-11-28 22:33:52 +0000

    Removed "extra" config keys from factory classes; the keys override the adapter keys when they should not.

--- Solar/Auth.php
+++ Solar/Auth.php
@@ -35,60 +35,11 @@ class Solar_Auth extends Solar_Base {
      * `adapter`
      * : (string) The adapter class, e.g. 'Solar_Auth_Adapter_File'.
      * 
-     * `config`
-     * : (array) Construction-time config keys to pass to the adapter
-     *   to override Solar.config.php values.  Default is null.
-     * 
-     * `expire`
-     * : (int) Authentication lifetime in seconds; zero is
-     *   forever.  Default is 14400 (4 hours).
-     * 
-     * `idle`
-     * : (int) Maximum allowed idle time in seconds; zero is
-     *   forever.  Default is 1800 (30 minutes).
-     * 
-     * `allow`
-     * : (bool) Whether or not to allow login/logout attempts.
-     * 
-     * `source`
-     * : (string) The source for auth credentials, 'get' (via the
-     *   for GET request vars) or 'post' (via the POST request vars).
-     *   Default is 'post'.
-     * 
-     * `source_handle`
-     * : (string) Username key in the credential array source,
-     *   default 'handle'.
-     * 
-     * `source_passwd`
-     * : (string) Password key in the credential array source,
-     *   default 'passwd'.
-     * 
-     * `source_submit`
-     * : (string) Submission key in the credential array source,
-     *   default 'submit'.
-     * 
-     * `submit_login`
-     * : (string) The submission-key value to indicate a
-     *   login attempt; default is the 'SUBMIT_LOGIN' locale key value.
-     * 
-     * `submit_logout`
-     * : (string) The submission-key value to indicate a
-     *   login attempt; default is the 'SUBMIT_LOGOUT' locale key value.
-     * 
      * @var array
      * 
      */
     protected $_Solar_Auth = array(
-        'adapter'       => 'Solar_Auth_Adapter_None',
-        'expire'        => 14400,
-        'idle'          => 1800,
-        'allow'         => true,
-        'source'        => 'post',
-        'source_handle' => 'handle',
-        'source_passwd' => 'passwd',
-        'source_submit' => 'submit',
-        'submit_login'  => null,
-        'submit_logout' => null,
+        'adapter' => 'Solar_Auth_Adapter_None',
     );
     
     /**

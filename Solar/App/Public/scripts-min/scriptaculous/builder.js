a08f04b5ce94221e909b94434be75b76be9b4812
Clay Loveless <clay@claylo.com>
2007-01-24 01:33:07 +0000

    Protaculous update
    - Prototype 1.5.0 final
    - script.aculo.us 1.7.0 final

--- Solar/App/Public/scripts-min/scriptaculous/builder.js
+++ Solar/App/Public/scripts-min/scriptaculous/builder.js
@@ -1,13 +1,13 @@
-// From script.aculo.us 1.6.5, compressed by jsmin (http://www.crockford.com/javascript/jsmin.html).
+// From script.aculo.us 1.7.0, compressed by jsmin (http://www.crockford.com/javascript/jsmin.html).
 
 var Builder={NODEMAP:{AREA:'map',CAPTION:'table',COL:'table',COLGROUP:'table',LEGEND:'fieldset',OPTGROUP:'select',OPTION:'select',PARAM:'object',TBODY:'table',TD:'table',TFOOT:'table',TH:'table',THEAD:'table',TR:'table'},node:function(elementName){elementName=elementName.toUpperCase();var parentTag=this.NODEMAP[elementName]||'div';var parentElement=document.createElement(parentTag);try{parentElement.innerHTML="<"+elementName+"></"+elementName+">";}catch(e){}
-var element=parentElement.firstChild||null;if(element&&(element.tagName!=elementName))
+var element=parentElement.firstChild||null;if(element&&(element.tagName.toUpperCase()!=elementName))
 element=element.getElementsByTagName(elementName)[0];if(!element)element=document.createElement(elementName);if(!element)return;if(arguments[1])
 if(this._isStringOrNumber(arguments[1])||(arguments[1]instanceof Array)){this._children(element,arguments[1]);}else{var attrs=this._attributes(arguments[1]);if(attrs.length){try{parentElement.innerHTML="<"+elementName+" "+
 attrs+"></"+elementName+">";}catch(e){}
 element=parentElement.firstChild||null;if(!element){element=document.createElement(elementName);for(attr in arguments[1])
 element[attr=='class'?'className':attr]=arguments[1][attr];}
-if(element.tagName!=elementName)
+if(element.tagName.toUpperCase()!=elementName)
 element=parentElement.getElementsByTagName(elementName)[0];}}
 if(arguments[2])
 this._children(element,arguments[2]);return element;},_text:function(text){return document.createTextNode(text);},ATTR_MAP:{'className':'class','htmlFor':'for'},_attributes:function(attributes){var attrs=[];for(attribute in attributes)

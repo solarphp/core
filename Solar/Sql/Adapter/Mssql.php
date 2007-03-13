4a3ca53fb7e97a846eb63c28af758b99537ad4b9
Paul M. Jones <pmjones@solarphp.com>
2007-03-13 00:36:09 +0000

    Solar_Sql_Adapter_Mssql: [BRK] Partial commit of MS-SQL changes, incl. work from Stefan Bogdan

--- Solar/Sql/Adapter/Mssql.php
+++ Solar/Sql/Adapter/Mssql.php
@@ -9,6 +9,8 @@
  * 
  * @author Paul M. Jones <pmjones@solarphp.com>
  * 
+ * @author Stefan Bogdan <stefan_bogdan_daniel@yahoo.com>
+ * 
  * @license http://opensource.org/licenses/bsd-license.php BSD
  * 
  * @version $Id$
@@ -42,12 +44,12 @@ class Solar_Sql_Adapter_Mssql extends Solar_Sql_Adapter {
      */
     protected $_native = array(
         'bool'      => 'BIT',
-        'char'      => 'BINARY(:size)',
-        'varchar'   => 'VARBINARY(:size)',
+        'char'      => 'CHAR',
+        'varchar'   => 'VARCHAR',
         'smallint'  => 'SMALLINT',
         'int'       => 'INTEGER',
         'bigint'    => 'BIGINT',
-        'numeric'   => 'DECIMAL(:size,:scope)',
+        'numeric'   => 'DECIMAL',
         'float'     => 'FLOAT',
         'clob'      => 'TEXT',
         'date'      => 'CHAR(10)',
@@ -66,17 +68,74 @@ class Solar_Sql_Adapter_Mssql extends Solar_Sql_Adapter {
     
     /**
      * 
-     * Adds a LIMIT clause (or equivalent) to a SELECT statement.
+     * Constructor.
      * 
-     * Still not great, but much more efficient than old way.
-     * http://lists.bestpractical.com/pipermail/rt-devel/2005-June/007339.html
+     * @param array $config User-provided configuration values.
      * 
-     * @param array $parts The SELECT statement parts.
+     */
+    public function __construct($config = null)
+    {
+        parent::__construct($config);
+        
+        // if we're on windows, use the 'mssql' PDO type
+    	if (substr(PHP_OS, 0, 3) == 'WIN') {
+    	    $this->_pdo_type = 'mssql';
+    	}
+    }
+    
+    /**
+     * 
+     * Creates a PDO-style DSN.
+     * 
+     * For example, "mysql:host=127.0.0.1;dbname=test"
+     * 
+     * @return string A PDO-style DSN.
+     * 
+     */
+    protected function _dsn()
+    {
+        // the default build process
+        if ($this->_pdo_type != 'mssql') {
+            return parent::_dsn();
+        }
+        
+        // special build process for 'mssql' pdo type on Windows
+        $dsn = array();
+        
+        // the host
+        if (! empty($this->_config['host'])) {
+            $tmp = 'host=' . $this->_config['host'];
+            // the port
+            if (! empty($this->_config['port'])) {
+    		    $tmp = $tmp . ',' . $this->_config['port'];
+            }
+            $dsn[] = $tmp;
+        }
+        
+        // database name
+        if (! empty($this->_config['name'])) {
+            $dsn[] = 'dbname=' . $this->_config['name'];
+        }
+	    
+	    // done!
+        return $this->_pdo_type . ':' . implode(';', $dsn);
+    }
+    
+    /**
+     * 
+     * Modifies a SELECT statement in place to add a LIMIT clause.
+     * 
+     * <http://lists.bestpractical.com/pipermail/rt-devel/2005-June/007339.html>
+     * 
+     * @param string &$stmt The SELECT statement.
+     * 
+     * @param array &$parts The orignal SELECT component parts, in case the
+     * adapter needs them.
      * 
      * @return void
      * 
      */
-    protected function _buildSelect($parts)
+    protected function _modSelect(&$stmt, &$parts)
     {
         // determine count
         $count = ! empty($parts['limit']['count'])
@@ -87,9 +146,6 @@ class Solar_Sql_Adapter_Mssql extends Solar_Sql_Adapter {
         $offset = ! empty($parts['limit']['offset'])
             ? (int) $parts['limit']['offset']
             : 0;
-        
-        // build the basic statement
-        $stmt = parent::_buildSelect($parts);
         
         // add limits?
         if ($count) {
@@ -144,9 +200,9 @@ class Solar_Sql_Adapter_Mssql extends Solar_Sql_Adapter {
     
     /**
      * 
-     * Returns a list of database tables.
+     * Returns a list of all tables in the database.
      * 
-     * @return array The list of tables in the database.
+     * @return array All table names in the database.
      * 
      */
     public function fetchTableList()
@@ -168,10 +224,54 @@ class Solar_Sql_Adapter_Mssql extends Solar_Sql_Adapter {
      */
     public function fetchTableCols($table)
     {
-        throw $this->_exception(
-            'ERR_METHOD_NOT_IMPLEMENTED',
-            array('method' => 'fetchTableCols')
-        );
+ 	    $cmd = "
+     	    SELECT
+                C.COLUMN_NAME AS name, 
+                C.DATA_TYPE AS type,
+                C.IS_NULLABLE AS allow_nulls,
+                C.NUMERIC_PRECISION AS size,
+                C.NUMERIC_SCALE AS scope,
+                (SELECT 't'
+                    FROM
+                        information_schema.key_column_usage AS k,
+                	    information_schema.table_constraints AS tc 
+                	WHERE
+                	    tc.constraint_name = k.constraint_name
+                	    AND tc.constraint_type = 'PRIMARY KEY'
+                	    AND k.table_name = :table
+                	    AND k.column_name  = c.COLUMN_NAME
+                ) AS primary_key,
+                c.COLUMN_DEFAULT,
+                COLUMNPROPERTY( OBJECT_ID('tabela'), C.COLUMN_NAME, 'IsIdentity') AS autoinc
+            FROM INFORMATION_SCHEMA.Tables T
+            JOIN INFORMATION_SCHEMA.Columns AS C ON T.TABLE_NAME = C.TABLE_NAME
+            WHERE
+                T.TABLE_NAME NOT LIKE 'sys%'
+                AND T.TABLE_NAME = :table
+        ";
+        
+        // where the description will be stored
+        $descr = array();
+        
+        // loop through the result rows; each describes a column.
+        $cols = $this->fetchAll($cmd, array('table' => $table));
+        foreach ($cols as $val) {
+            $name = $val['name'];
+            list($type, $size, $scope) = $this->_getTypeSizeScope($val['type']);
+            $descr[$name] = array(
+                'name'    => $name,
+                'type'    => $type,
+                'size'    => $val['size'],
+                'scope'   => $val['scope'],
+                'default' => $this->_getDefault($val['column_default']),
+                'require' => (bool) ($val['allow_nulls'] == 'NO'),
+                'primary' => (bool) ($val['primary_key'] == 't'),
+                'autoinc' => (bool) ($val['autoinc'] == 1),
+            );
+        }
+        
+        // done
+        return $descr;
     }
     
     /**
@@ -189,6 +289,22 @@ class Solar_Sql_Adapter_Mssql extends Solar_Sql_Adapter {
     {
         // http://www.w3schools.com/sql/sql_drop.asp
         $this->query("DROP INDEX {$table}.{$name}");
+    }
+    
+    /**
+     * 
+     * Get the last auto-incremented insert ID from the database.
+     * 
+     * @param string $name The name of the auto-increment series; optional,
+     * not normally required.
+     * 
+     * @return int The last auto-increment ID value inserted to the database.
+     * 
+     */
+    public function lastInsertId($name = null)
+    {
+        $this->_connect();
+	    return $this->fetchValue('SELECT @@IDENTITY AS id');
     }
     
     /**
@@ -260,5 +376,37 @@ class Solar_Sql_Adapter_Mssql extends Solar_Sql_Adapter {
         
         // return the sequence number
         return $id;
+    }
+    
+    /**
+     * 
+     * Given a native column SQL default value, finds a PHP literal value.
+     * 
+     * SQL NULLs are converted to PHP nulls.  Non-literal values (such as
+     * keywords and functions) are also returned as null.
+     * 
+     * @param string $default The column default SQL value.
+     * 
+     * @return scalar A literal PHP value.
+     * 
+     */
+    protected function _getDefault($default)
+    {
+    	$default = str_replace(
+    	    array('(', ')'),
+    	    '',
+    	    $default
+    	);
+    	
+        // numeric literal?
+        if (is_numeric($default)) {
+            return $default;
+        }
+	
+    	if ($default == NULL) {
+    		return null;
+    	}
+    	
+    	return $default;
     }
 }

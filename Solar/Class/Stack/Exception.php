4b24aa76318b602b94ca3fda53d18b7b34a43bf1
Paul M. Jones <pmjones@solarphp.com>
2007-03-28 22:42:13 +0000

    updated comments

--- Solar/Class/Stack/Exception.php
+++ Solar/Class/Stack/Exception.php
@@ -1,2 +1,27 @@
 <?php
+/**
+ * 
+ * Generic class-stack exception.
+ * 
+ * @category Solar
+ * 
+ * @package Solar
+ * 
+ * @author Paul M. Jones <pmjones@solarphp.com>
+ * 
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ * 
+ * @version $Id$
+ * 
+ */
+
+/**
+ * 
+ * Generic class-stack exception.
+ * 
+ * @category Solar
+ * 
+ * @package Solar
+ * 
+ */
 class Solar_Class_Stack_Exception extends Solar_Exception {}
--- Solar/Class/Stack/Exception/ClassNotFound.php
+++ Solar/Class/Stack/Exception/ClassNotFound.php
@@ -1,2 +1,27 @@
 <?php
+/**
+ * 
+ * Class-stack exception: the requested class name was not found in the stack.
+ * 
+ * @category Solar
+ * 
+ * @package Solar
+ * 
+ * @author Paul M. Jones <pmjones@solarphp.com>
+ * 
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ * 
+ * @version $Id$
+ * 
+ */
+
+/**
+ * 
+ * Class-stack exception: the requested class name was not found in the stack.
+ * 
+ * @category Solar
+ * 
+ * @package Solar
+ * 
+ */
 class Solar_Class_Stack_Exception_ClassNotFound extends Solar_Class_Stack_Exception {}

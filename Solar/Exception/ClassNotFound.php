99ec856f7c99de3efe5613f3ecd5eee351b63f84
Paul M. Jones <pmjones@solarphp.com>
2007-03-28 18:04:03 +0000

    [NEW] Solar_Exception_ClassNotFound

---
+++ Solar/Exception/ClassNotFound.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * 
+ * Exception: a requested class was not found.
+ * 
+ * @category Solar
+ * 
+ * @package Solar
+ * 
+ * @author Paul M. Jones <pmjones@solarphp.com>
+ * 
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ * 
+ * @version $Id$
+ * 
+ */
+
+/**
+ * 
+ * Exception: a requested class was not found.
+ * 
+ * @category Solar
+ * 
+ * @package Solar
+ * 
+ */
+class Solar_Exception_ClassNotFound extends Solar_Exception {}

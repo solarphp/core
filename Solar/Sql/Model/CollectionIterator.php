df2db70d39c8f171e982668ff6129ef8173973a4
Paul M. Jones <pmjones@solarphp.com>
2007-03-30 04:35:04 +0000

    added comments

--- Solar/Sql/Model/CollectionIterator.php
+++ Solar/Sql/Model/CollectionIterator.php
@@ -1,4 +1,29 @@
 <?php
+/**
+ * 
+ * Iterator aggregate for record collections.
+ * 
+ * @category Solar
+ * 
+ * @package Solar_Sql_Model
+ * 
+ * @author Paul M. Jones <pmjones@solarphp.com>
+ * 
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ * 
+ * @version $Id: Table.php 2093 2007-01-15 15:13:12Z pmjones $
+ * 
+ */
+
+/**
+ * 
+ * Iterator aggregate for record collections.
+ * 
+ * @category Solar
+ * 
+ * @package Solar_Sql_Model
+ * 
+ */
 class Solar_Sql_Model_CollectionIterator extends Solar_Sql_Model_RecordIterator {
     
     /**

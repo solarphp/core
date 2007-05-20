e56eebfb24229601c6d7b5e5eff4d0e5d9f31800
Clay Loveless <clay@claylo.com>
2007-05-20 14:00:14 +0000

    Solar_View_Helper_Js: [CHG] Reverting independent script block change.
    - Want to think about this some more, backing out for now.

--- Solar/View/Helper/Js.php
+++ Solar/View/Helper/Js.php
@@ -195,9 +195,9 @@ class Solar_View_Helper_Js extends Solar_View_Helper_JsLibrary {
 
         // Gather all registered scripts for output
         if (!empty($this->scripts)) {
-            foreach ($this->scripts as $script) {
-                $js .= $this->_view->inlineScript($script);
-            }
+            $scripts = implode("\n\n", $this->scripts);
+            $scripts = trim($scripts);
+            $js = $this->_view->inlineScript($scripts);
         }
 
         return $js;

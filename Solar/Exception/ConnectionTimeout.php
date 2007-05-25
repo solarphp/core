2c9272e7e933aea3425dc117632e15e9b58734c7
Paul M. Jones <pmjones@solarphp.com>
2007-05-25 16:34:47 +0000

    Solar_Exception_ConnectionTimeout: [NEW] A special kind of connection failure exception to use when the connection times out.

---
+++ Solar/Exception/ConnectionTimeout.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * 
+ * Exception: connection to a resource timed out.
+ * 
+ * @category Solar
+ * 
+ * @package Solar
+ * 
+ * @author Paul M. Jones <pmjones@solarphp.com>
+ * 
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ * 
+ * @version $Id: ConnectionFailed.php 2440 2007-04-21 14:33:44Z pmjones $
+ * 
+ */
+
+/**
+ * 
+ * Exception: connection to a resource timed out.
+ * 
+ * @category Solar
+ * 
+ * @package Solar
+ * 
+ */
+class Solar_Exception_ConnectionTimeout extends Solar_Exception {}

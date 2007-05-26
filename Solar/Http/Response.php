60564ce17e65641daf425069cd2efaba797741de
Paul M. Jones <pmjones@solarphp.com>
2007-05-26 19:58:03 +0000

    Rename Solar_Response to Solar_Http_Response
    Solar_Response
    --------------
    
    * [DEL] Renamed to Solar_Http_Response
    
    
    Solar_Http_Response
    -------------------
    
    * [NEW] Renamed from Solar_Response.
    
    * [BRK] Property $body is now $content.
    
    * [BRK] Method setStatus() is split into two methods, setStatusCode() and 
      setStatusText().
    
    * [BRK] Method send() is now named display().

---
+++ Solar/Http/Response.php
@@ -0,0 +1,510 @@
+<?php
+/**
+ * 
+ * Generic HTTP response object for sending headers, cookies, and content.
+ * 
+ * @category Solar
+ * 
+ * @package Solar_Http
+ * 
+ * @author Paul M. Jones <pmjones@solarphp.com>
+ * 
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ * 
+ * @version $Id$
+ * 
+ */
+
+/**
+ * 
+ * Generic HTTP response object for sending headers, cookies, and content.
+ * 
+ * @category Solar
+ * 
+ * @package Solar_Http
+ * 
+ * @todo Add charset param so that headers get sent with right encoding?
+ * 
+ */
+class Solar_Http_Response extends Solar_Base {
+    
+    /**
+     * 
+     * The response body content.
+     * 
+     * @var string
+     * 
+     */
+    public $content = null;
+    
+    /**
+     * 
+     * All headers to send at display() time.
+     * 
+     * @var array
+     * 
+     */
+    protected $_headers = array();
+    
+    /**
+     * 
+     * All cookies to send at display() time.
+     * 
+     * @var array
+     * 
+     */
+    protected $_cookies = array();
+    
+    /**
+     * 
+     * Whether or not cookies should default being sent by HTTP only.
+     * 
+     * @var bool
+     * 
+     */
+    protected $_cookies_httponly = true;
+    
+    /**
+     * 
+     * The HTTP response status code.
+     * 
+     * @var int
+     * 
+     */
+    protected $_status_code = 200;
+    
+    /**
+     * 
+     * The HTTP response status text.
+     * 
+     * @var int
+     * 
+     */
+    protected $_status_text = null;
+    
+    /**
+     * 
+     * The HTTP version to send as.
+     * 
+     * @var string
+     * 
+     */
+    protected $_version = '1.1';
+    
+    /**
+     * 
+     * Sends all headers and cookies, then returns the body.
+     * 
+     * @return string
+     * 
+     */
+    public function __toString()
+    {
+        $this->_sendHeaders();
+        return $this->content;
+    }
+    
+    /**
+     * 
+     * Sets the HTTP version to '1.0' or '1.1'.
+     * 
+     * @param string $version The HTTP version to use for this response.
+     * 
+     * @return void
+     * 
+     * @throws Solar_Http_Response_Exception_HttpVersion when the version number
+     * is not '1.0' or '1.1'.
+     * 
+     */
+    public function setVersion($version)
+    {
+        $version = trim($version);
+        if ($version != '1.0' && $version != '1.1') {
+            throw $this->_exception('ERR_HTTP_VERSION', array(
+                'version' => $version
+            ));
+        } else {
+            $this->_version = $version;
+        }
+    }
+    
+    /**
+     * 
+     * Returns the HTTP version for this response.
+     * 
+     * @return string
+     * 
+     */
+    public function getVersion()
+    {
+        return $this->_version;
+    }
+    
+    /**
+     * 
+     * Sets the HTTP response status code.
+     * 
+     * Automatically resets the status text to the default for this code.
+     * 
+     * @param int $code An HTTP status code, such as 200, 302, 404, etc.
+     * 
+     * @return void
+     * 
+     */
+    public function setStatusCode($code)
+    {
+        $code = (int) $code;
+        if ($code < 100 || $code > 599) {
+            throw $this->_exception('ERR_STATUS_CODE', array(
+                'code' => $code,
+            ));
+        }
+        
+        $this->_status_code = $code;
+        $this->setStatusText(null);
+    }
+    
+    /**
+     * 
+     * Sets the HTTP response status text.
+     * 
+     * @param string $text The status text; if empty, will set the text to the
+     * default for the current status code.
+     * 
+     * @return void
+     * 
+     */
+    public function setStatusText($text)
+    {
+        // trim and remove newlines from custom text
+        $text = trim(str_replace(array("\r", "\n"), '', $text));
+        if ($text) {
+            // use custom text
+            $this->_status_text = $text;
+        } else {
+            // use default text for status code
+            $this->_status_text = $this->locale("STATUS_{$this->_status_code}");
+        }
+    }
+    
+    /**
+     * 
+     * Returns the current status code.
+     * 
+     * @return int
+     * 
+     */
+    public function getStatusCode()
+    {
+        return $this->_status_code;
+    }
+    
+    /**
+     * 
+     * Returns the current status text.
+     * 
+     * @return string
+     * 
+     */
+    public function getStatusText()
+    {
+        return $this->_status_text;
+    }
+    
+    /**
+     * 
+     * By default, should cookies be sent by HTTP only?
+     * 
+     * @param bool $flag True to send by HTTP only, false to send by any
+     * method.
+     * 
+     * @return void
+     * 
+     */
+    public function setCookiesHttponly($flag)
+    {
+        $this->_cookies_httponly = (bool) $flag;
+    }
+    
+    /**
+     * 
+     * Sets a header value in $this->_headers; will be sent to the client at
+     * display() time.
+     * 
+     * This method will not set 'HTTP' headers for response status codes; use
+     * the [[setStatusCode()]] and [[setStatusText()]] methods instead.
+     * 
+     * @param string $key The header label, such as "Content-Type".
+     * 
+     * @param string $val The value for the header.
+     * 
+     * @param bool $replace This header value should replace any previous
+     * values of the same key.  When false, the same header key is sent
+     * multiple times with the different values.
+     * 
+     * @return void
+     * 
+     * @throws Solar_Http_Response_Exception_HeadersSent when headers have
+     * already been sent.
+     * 
+     * @see [[php::header() | ]]
+     * 
+     */
+    public function setHeader($key, $val, $replace = true)
+    {
+        // have headers been sent already?
+        $this->_checkHeadersSent();
+        
+        // normalize the header key
+        $key = Solar_Mime::headerLabel($key);
+        
+        // disallow HTTP header
+        $lower = strtolower($key);
+        if ($lower == 'http') {
+            return;
+        }
+        
+        // add the header to the list
+        if ($replace || empty($this->_headers[$key])) {
+            // replacement, or first instance of the key
+            $this->_headers[$key] = $val;
+        } else {
+            // second or later instance of the key
+            settype($this->_headers[$key], 'array');
+            $this->_headers[$key][] = $val;
+        }
+    }
+    
+    /**
+     * 
+     * Returns the value of a single header.
+     * 
+     * @param string $key The header name.
+     * 
+     * @return string|array A string if the header has only one value, or an
+     * array if the header has multiple values, or null if the header does not
+     * exist.
+     * 
+     */
+    public function getHeader($key)
+    {
+        // normalize the header key
+        $key = Solar_Mime::headerLabel($key);
+        
+        // get the value
+        if (! empty($this->_header[$key])) {
+            return $this->_header[$key];
+        }
+    }
+    
+    /**
+     * 
+     * Returns the array of all headers to be sent with the response.
+     * 
+     * @return array
+     * 
+     */
+    public function getHeaders()
+    {
+        return $this->_headers;
+    }
+    
+    /**
+     * 
+     * Sets a cookie value in $this->_cookies; will be sent to the client at
+     * display() time.
+     * 
+     * @param string $name The name of the cookie.
+     * 
+     * @param string $value The value of the cookie.
+     * 
+     * @param int|string $expire The Unix timestamp after which the cookie
+     * expires.  If non-numeric, the method uses strtotime() on the value.
+     * 
+     * @param string $path The path on the server in which the cookie will be
+     * available on.
+     * 
+     * @param string $domain The domain that the cookie is available on.
+     * 
+     * @param bool $secure Indicates that the cookie should only be
+     * transmitted over a secure HTTPS connection.
+     * 
+     * @param bool $httponly When true, the cookie will be made accessible
+     * only through the HTTP protocol. This means that the cookie won't be
+     * accessible by scripting languages, such as JavaScript.
+     * 
+     * @return void
+     * 
+     * @see [[php::setcookie() | ]]
+     * 
+     * @throws Solar_Http_Response_Exception_HeadersSent when headers have
+     * already been sent.
+     * 
+     */
+    public function setCookie($name, $value = '', $expire = 0,
+        $path = '', $domain = '', $secure = false, $httponly = null)
+    {
+        // have headers been sent already?
+        $this->_checkHeadersSent();
+        
+        // store the cookie value
+        $this->_cookies[$name] = array(
+            'value'     => $value,
+            'expire'    => $expire,
+            'path'      => $path,
+            'domain'    => $domain,
+            'secure'    => $secure,
+            'httponly'  => $httponly,
+        );
+    }
+    
+    /**
+     * 
+     * Returns the value and options for a single cookie.
+     * 
+     * @param string $key The cookie name.
+     * 
+     * @return array An array of the value and options for the cookie.
+     * 
+     */
+    public function getCookie($key)
+    {
+        if (! empty($this->_cookie[$key])) {
+            return $this->_cookie[$key];
+        }
+    }
+    
+    /**
+     * 
+     * Returns the array of cookies that will be set by the response.
+     * 
+     * @return array
+     * 
+     */
+    public function getCookies()
+    {
+        return $this->_cookies;
+    }
+    
+    /**
+     * 
+     * Sends all headers and cookies, then prints the response content.
+     * 
+     * @return void
+     * 
+     */
+    public function display()
+    {
+        $this->_sendHeaders();
+        echo $this->content;
+    }
+    
+    /**
+     * 
+     * Checks to see if headers have been sent, throws an exception if they
+     * have.
+     * 
+     * @return void
+     * 
+     * @throws Solar_Http_Response_Exception_HeadersSent when headers have
+     * already been sent.
+     * 
+     */
+    protected function _checkHeadersSent()
+    {
+        // have headers been sent already?
+        if (headers_sent($file, $line)) {
+            throw $this->_exception('ERR_HEADERS_SENT', array(
+                'file' => $file,
+                'line' => $line,
+            ));
+        }
+    }
+    
+    /**
+     * 
+     * Sends all headers and cookies.
+     * 
+     * @return void
+     * 
+     * @throws Solar_Http_Response_Exception_HeadersSent when headers have
+     * already been sent.
+     * 
+     */
+    protected function _sendHeaders()
+    {
+        // has this response already been sent?
+        $this->_checkHeadersSent();
+        
+        // build the full status header string
+        $status = "HTTP/{$this->_version} {$this->_status_code}";
+        if ($this->_status_text) {
+            $status .= " {$this->_status_text}";
+        }
+        
+        // send the status header
+        header($status, true, $this->_status_code);
+        
+        // send each of the remaining headers
+        foreach ($this->_headers as $key => $list) {
+            
+            // sanitize and skip empty keys
+            $key = Solar_Mime::headerLabel($key);
+            if (! $key) {
+                continue;
+            }
+            
+            // set each value for the header
+            foreach ((array) $list as $val) {
+                // sanitize and set
+                $line = Solar_Mime::headerLine($key, $val);
+                header($line);
+            }
+        }
+        
+        // send each of the cookies
+        foreach ($this->_cookies as $key => $val) {
+            
+            // was httponly set for this cookie?  if not,
+            // use the default.
+            $httponly = ($val['httponly'] === null)
+                ? $this->_cookies_httponly
+                : (bool) $val['httponly'];
+            
+            // try to allow for times not in unix-timestamp format
+            if (! is_numeric($val['expire'])) {
+                $val['expire'] = strtotime($val['expire']);
+            }
+            
+            // actually set the cookie
+            setcookie(
+                $key,
+                $val['value'],
+                (int) $val['expire'],
+                $val['path'],
+                $val['domain'],
+                (bool) $val['secure'],
+                (bool) $httponly
+            );
+        }
+    }
+    
+    /**
+     * 
+     * Dumps the values of this object.
+     * 
+     * @return void
+     * 
+     */
+    public function dump($var = null, $label = null)
+    {
+        if ($var) {
+            return parent::dump($var, $label);
+        } else {
+            $clone = clone($this);
+            unset($clone->_datafilter);
+            unset($clone->_config);
+            return parent::dump($clone, $label);
+        }
+    }
+}
---
+++ Solar/Http/Response/Locale/en_US.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * 
+ * Locale file to return the strings for English/US.
+ * 
+ * @category Solar
+ * 
+ * @package Solar_Http
+ * 
+ * @author Paul M. Jones <pmjones@solarphp.com>
+ * 
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ * 
+ * @version $Id$
+ * 
+ */
+return array(
+    'STATUS_100' => 'Continue',
+    'STATUS_101' => 'Switching Protocols',
+    
+    'STATUS_200' => 'OK',
+    'STATUS_201' => 'Created',
+    'STATUS_202' => 'Accepted',
+    'STATUS_203' => 'Non-Authoritative Information',
+    'STATUS_204' => 'No Content',
+    'STATUS_205' => 'Reset Content',
+    'STATUS_206' => 'Partial Content',
+    
+    'STATUS_300' => 'Multiple Choices',
+    'STATUS_301' => 'Moved Permanently',
+    'STATUS_302' => 'Found',
+    'STATUS_303' => 'See Other',
+    'STATUS_304' => 'Not Modified',
+    'STATUS_305' => 'Use Proxy',
+    'STATUS_307' => 'Temporary Redirect',
+    
+    'STATUS_400' => 'Bad Request',
+    'STATUS_401' => 'Unauthorized',
+    'STATUS_402' => 'Payment Required',
+    'STATUS_403' => 'Forbidden',
+    'STATUS_404' => 'Not Found',
+    'STATUS_405' => 'Method Not Allowed',
+    'STATUS_406' => 'Not Acceptable',
+    'STATUS_407' => 'Proxy Authentication Required',
+    'STATUS_408' => 'Request Timeout',
+    'STATUS_409' => 'Conflict',
+    'STATUS_410' => 'Gone',
+    'STATUS_411' => 'Length Required',
+    'STATUS_412' => 'Precondition Failed',
+    'STATUS_413' => 'Request Entity Too Large',
+    'STATUS_414' => 'Request Uri Too Long',
+    'STATUS_415' => 'Unsupported Media Type',
+    'STATUS_416' => 'Requested Range Not Satisfiable',
+    'STATUS_417' => 'Expectation Failed',
+    
+    'STATUS_500' => 'Internal Server Error',
+    'STATUS_501' => 'Not Implemented',
+    'STATUS_502' => 'Bad Gateway',
+    'STATUS_503' => 'Service Unavailable',
+    'STATUS_504' => 'Gateway Timeout',
+    'STATUS_505' => 'HTTP Version Not Supported',
+);

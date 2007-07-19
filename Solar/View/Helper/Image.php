6436f9f3a14a86f5a07cb24f5778c0a54fca65b2
Paul M. Jones <pmjones@solarphp.com>
2007-07-19 20:40:37 +0000

    tabs to 4-spaced

--- Solar/View/Helper/Image.php
+++ Solar/View/Helper/Image.php
@@ -26,31 +26,31 @@
  * 
  */
 class Solar_View_Helper_Image extends Solar_View_Helper {
-	
-	/**
-	 * 
-	 * Returns an <img ... /> tag.
-	 * 
-	 * If an "alt" attribute is not specified, will add it from the
-	 * image [[php::basename() | ]].
-	 * 
-	 * @param string $src The href to the image source.
-	 * 
-	 * @param array $attribs Additional attributes for the tag.
-	 * 
-	 * @return string An <img ... /> tag.
-	 * 
-	 * @todo Add automated height/width calculation?
-	 * 
-	 */
-	public function image($src, $attribs = array())
-	{
-	    unset($attribs['src']);
-	    if (empty($attribs['alt'])) {
-	        $attribs['alt'] = basename($src);
-	    }
-	    
-	    return '<img src="' . $this->_view->publicHref($src) . '"'
-	         . $this->_view->attribs($attribs) . ' />';
-	}
+    
+    /**
+     * 
+     * Returns an <img ... /> tag.
+     * 
+     * If an "alt" attribute is not specified, will add it from the
+     * image [[php::basename() | ]].
+     * 
+     * @param string $src The href to the image source.
+     * 
+     * @param array $attribs Additional attributes for the tag.
+     * 
+     * @return string An <img ... /> tag.
+     * 
+     * @todo Add automated height/width calculation?
+     * 
+     */
+    public function image($src, $attribs = array())
+    {
+        unset($attribs['src']);
+        if (empty($attribs['alt'])) {
+            $attribs['alt'] = basename($src);
+        }
+        
+        return '<img src="' . $this->_view->publicHref($src) . '"'
+             . $this->_view->attribs($attribs) . ' />';
+    }
 }

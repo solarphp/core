a2ecf6f097889e3c34fdac1e0ad4e24b7ff8115d
Paul M. Jones <pmjones@solarphp.com>
2007-10-12 16:45:37 +0000

    added Solar_Model_Nodes_Collection for extension

---
+++ Solar/Model/Nodes/Collection.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * 
+ * A collection of records from the "nodes" model.
+ * 
+ * @category Solar
+ * 
+ * @package Solar_Model
+ * 
+ * @author Paul M. Jones <pmjones@solarphp.com>
+ * 
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ * 
+ * @version $Id: Exception.php 2804 2007-10-06 14:01:27Z pmjones $
+ * 
+ */
+
+/**
+ * 
+ * A collection of records from the "nodes" model.
+ * 
+ * @category Solar
+ * 
+ * @package Solar_Model
+ * 
+ */
+class Solar_Model_Nodes_Bookmarks_Collection extends Solar_Model_Nodes_Collection {}

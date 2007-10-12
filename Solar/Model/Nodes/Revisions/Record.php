df1c713d217706939e08380a24fc1027477deb59
Paul M. Jones <pmjones@solarphp.com>
2007-10-12 16:00:55 +0000

    moved unused model node-types to holding location

--- Solar/Model/Nodes/Revisions/Record.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Solar_Model_Nodes_Revisions_Record extends Solar_Sql_Model_Record {
-    protected function __getId()
-    {
-        echo "I am the ID!";
-        return $this->_data['id'];
-    }
-}

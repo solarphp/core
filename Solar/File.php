04ab326382835521192e46d1e1274ddc4e8fd2fc
Paul M. Jones <pmjones@solarphp.com>
2007-10-17 13:32:00 +0000

    actually add the registry, dir, and file classes

---
+++ Solar/File.php
@@ -0,0 +1,105 @@
+<?php
+class Solar_File {
+    
+    protected static $_file;
+    
+    /**
+     * 
+     * Hack for [[php::file_exists() | ]] that checks the include_path.
+     * 
+     * Use this to see if a file exists anywhere in the include_path.
+     * 
+     * {{code: php
+     *     $file = 'path/to/file.php';
+     *     if (Solar_File::exists('path/to/file.php')) {
+     *         include $file;
+     *     }
+     * }}
+     * 
+     * @param string $file Check for this file in the include_path.
+     * 
+     * @return mixed If the file exists and is readble in the include_path,
+     * returns the path and filename; if not, returns boolean false.
+     * 
+     */
+    public static function exists($file)
+    {
+        // no file requested?
+        $file = trim($file);
+        if (! $file) {
+            return false;
+        }
+        
+        // using an absolute path for the file?
+        // dual check for Unix '/' and Windows '\',
+        // or Windows drive letter and a ':'.
+        $abs = ($file[0] == '/' || $file[0] == '\\' || $file[1] == ':');
+        if ($abs && file_exists($file)) {
+            return $file;
+        }
+        
+        // using a relative path on the file
+        $path = explode(PATH_SEPARATOR, ini_get('include_path'));
+        foreach ($path as $base) {
+            // strip Unix '/' and Windows '\'
+            $target = rtrim($base, '\\/') . DIRECTORY_SEPARATOR . $file;
+            if (file_exists($target)) {
+                return $target;
+            }
+        }
+        
+        // never found it
+        return false;
+    }
+    
+    /**
+     * 
+     * Returns the OS-specific directory for temporary files, optionally with
+     * a path added to it.
+     * 
+     * @param string $add Add this to the end of the temporary directory
+     * path.
+     * 
+     * @return string The temp directory path, with optional suffix added.
+     * 
+     */
+    public static function tmp($file)
+    {
+        // convert slashes to OS-specific separators,
+        // then remove leading and trailing separators
+        $file = str_replace('/', DIRECTORY_SEPARATOR, $file);
+        $file = trim($file, DIRECTORY_SEPARATOR);
+        
+        // return the tmp dir plus file name
+        return Solar_Dir::tmp() . $file;
+    }
+    
+    /**
+     * 
+     * Uses [[php::include() | ]] to run a script in a limited scope.
+     * 
+     * @param string $file The file to include.
+     * 
+     * @return mixed The return value of the included file.
+     * 
+     */
+    public static function load($file)
+    {
+        Solar_File::$_file = Solar_File::exists($file);
+        if (! Solar_File::$_file) {
+            // could not open the file for reading
+            throw Solar::exception(
+                'Solar_File',
+                'ERR_FILE_NOT_READABLE',
+                'File does not exist or is not readable',
+                array('file' => $file)
+            );
+        }
+        
+        // clean up the local scope, then include the file and
+        // return its results.
+        unset($file);
+        return include Solar_File::$_file;
+    }
+    
+}

dcb2000bdc7bd38aab920d99260606c5e40dc9fc
Paul M. Jones <pmjones@solarphp.com>
2007-10-18 19:36:20 +0000

    Solar_Exception_AutoloadFailed: [NEW] Thrown when autload() can't find a file

---
+++ Solar/Exception/AutoloadFailed.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * 
+ * Exception: autoload could not find a file.
+ * 
+ * @category Solar
+ * 
+ * @package Solar
+ * 
+ * @author Paul M. Jones <pmjones@solarphp.com>
+ * 
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ * 
+ * @version $Id: ConnectionFailed.php 2440 2007-04-21 14:33:44Z pmjones $
+ * 
+ */
+
+/**
+ * 
+ * Exception: autoload could not find a file.
+ * 
+ * @category Solar
+ * 
+ * @package Solar
+ * 
+ */
+class Solar_Exception_AutoloadFailed extends Solar_Exception {}

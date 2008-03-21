bdd723737362c413142e4252f62728e651929ebd
Paul M. Jones <pmjones@solarphp.com>
2008-03-21 17:02:27 +0000

    Solar/AppBase/Layout: '_head' partial now includes pager CSS file.

--- Solar/AppBase/Layout/_head.php
+++ Solar/AppBase/Layout/_head.php
@@ -39,7 +39,8 @@
                  ->addStyleBase("Solar/styles/cssfw/forms.css")
                  ->addStyleBase("Solar/styles/cssfw/layout-{$this->layout}.css")
                  ->addStyleBase("Solar/styles/typo.css")
-                 ->addStyleBase("Solar/styles/forms.css");
+                 ->addStyleBase("Solar/styles/forms.css")
+                 ->addStyleBase("Solar/styles/pager.css");
     
     // additional baseline styles
     foreach ((array) $this->layout_head['style'] as $val) {

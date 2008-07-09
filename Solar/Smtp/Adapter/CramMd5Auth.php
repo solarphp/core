8185f4725199241bddb445ef437333375fae1a4a
Paul M. Jones <pmjones@solarphp.com>
2008-07-09 18:14:30 +0000

    Solar_Smtp_Adapter_CramM5Auth: [FIX] Apparently hash_hmac() doesn't work for this; adding code lifted from Zend Framework to handle it.

--- Solar/Smtp/Adapter/CramMd5Auth.php
+++ Solar/Smtp/Adapter/CramMd5Auth.php
@@ -33,7 +33,7 @@ class Solar_Smtp_Adapter_CramMd5Auth extends Solar_Smtp_Adapter_PlainAuth
             $challenge = base64_decode($challenge);
             
             // send the password hashed with the server challenge
-            $hash = hash_hmac('md5', $this->_password, $challenge); 
+            $hash = $this->_hashHmacMd5($this->_password, $challenge); 
             $this->_send(base64_encode($this->_username . ' ' . $hash));
             $this->_expect(235);
             
@@ -42,5 +42,40 @@ class Solar_Smtp_Adapter_CramMd5Auth extends Solar_Smtp_Adapter_PlainAuth
         }
         
         return $this->_auth;
+    }
+    
+    /**
+     * 
+     * Prepare a hashed response to server challenge. Apparently hash_hmac()
+     * doesn't do the trick.
+     * 
+     * Adapted with minor naming changes from the Zend Framework class method
+     * Zend_Mail_Protocol_Smtp_Auth_Crammd5::_hmacMd5.
+     * 
+     * @param string $password The password to hash.
+     * 
+     * @param string $challenge The server challenge.
+     * 
+     * @return string The hashed response to the challenge.
+     * 
+     * @see [[php::hash_hmac() | ]]
+     * 
+     * @see 
+     */
+    protected function _hashHmacMd5($password, $challenge)
+    {
+        if (strlen($password) > 64) {
+            $password = pack('H32', hash('md5', $password));
+        } elseif (strlen($password) < 64) {
+            $password = str_pad($password, 64, chr(0));
+        }
+        
+        $k_ipad = substr($password, 0, 64) ^ str_repeat(chr(0x36), 64);
+        $k_opad = substr($password, 0, 64) ^ str_repeat(chr(0x5C), 64);
+        
+        $inner = pack('H32', hash('md5', $k_ipad . $challenge));
+        $hash = hash('md5', $k_opad . $inner);
+        
+        return $hash;
     }
 }

2c2cf1118cb54d6ed78acca5cf8458a7160ce54a
Paul M. Jones <pmjones@solarphp.com>
2008-07-30 14:53:01 +0000

    Solar_View_Helper_ActionHref: [NEW] Helper to work with action 'href' values directly, instead of via Solar_View_Helper_Action.  Thanks, Jeff Moore, for the report that led to this.

---
+++ Solar/View/Helper/ActionHref.php
@@ -0,0 +1,50 @@
+<?php
+class Solar_View_Helper_ActionHref extends Solar_View_Helper
+{
+    /**
+     * 
+     * Internal URI object for creating links.
+     * 
+     * @var Solar_Uri_Action
+     * 
+     */
+    protected $_uri = null;
+    
+    /**
+     * 
+     * Constructor.
+     * 
+     * @param array $config User-specified configuration.
+     * 
+     */
+    public function __construct($config = null)
+    {
+        // do the real configuration
+        parent::__construct($config);
+        
+        // get a URI processor
+        $this->_uri = Solar::factory('Solar_Uri_Action');
+    }
+    
+    /**
+     * 
+     * Returns an escaped href or src attribute value for an action URI.
+     * 
+     * @param Solar_Uri_Action|string $spec The href or src specification.
+     * 
+     * @return string
+     * 
+     */
+    public function actionHref($spec)
+    {
+        if ($spec instanceof Solar_Uri_Action) {
+            // already an action uri object
+            $href = $spec->get();
+        } else {
+            // build-and-fetch the string as an action spec
+            $href = $this->_uri->quick($spec);
+        }
+        
+        return $this->_view->escape($href);
+    }
+}

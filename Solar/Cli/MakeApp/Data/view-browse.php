8e344c27132fe9bc2b7af051e1d8be05ad81b3d0
Paul M. Jones <pmjones@solarphp.com>
2008-08-02 20:21:39 +0000

    Solar_Cli_MakeApp: [FIX] In 'browse' template, show different text when there are no records.

--- Solar/Cli/MakeApp/Data/view-browse.php
+++ Solar/Cli/MakeApp/Data/view-browse.php
@@ -1,30 +1,38 @@
 <h2><?php echo $this->getText('HEADING_BROWSE'); ?></h2>
 
-<?php
-    $pager = $this->pager($this->list->getPagerInfo());
-    echo $pager . "<br />";
-?>
+<?php if (! $this->list): ?>
 
-<ol>
-<?php foreach ($this->list as $item): ?>
-    <li><ul>
-    <?php
-        foreach ($item as $key => $val) {
-            echo "<li>" . $this->escape($key) . ": "
-               . $this->escape($val)
-               . "</li>\n";
-        }
+    <?php echo $this->getText('ERR_NO_RECORDS'); ?>
+
+<?php else: ?>
     
-        $id = $item->getPrimaryVal();
-        
-        echo "<li>"
-           . $this->action("{$this->controller}/read/$id", 'ACTION_READ')
-           . "</li>\n";
+    <?php
+        $pager = $this->pager($this->list->getPagerInfo());
+        echo $pager . "<br />";
     ?>
-    </ul></li>
-<?php endforeach; ?>
-</ol>
 
-<?php echo $pager . "<br />"; ?>
+    <ol>
+    <?php foreach ($this->list as $item): ?>
+        <li><ul>
+        <?php
+            foreach ($item as $key => $val) {
+                echo "<li>" . $this->escape($key) . ": "
+                   . $this->escape($val)
+                   . "</li>\n";
+            }
+    
+            $id = $item->getPrimaryVal();
+        
+            echo "<li>"
+               . $this->action("{$this->controller}/read/$id", 'ACTION_READ')
+               . "</li>\n";
+        ?>
+        </ul></li>
+    <?php endforeach; ?>
+    </ol>
+
+    <?php echo $pager . "<br />"; ?>
+
+<?php endif; ?>
 
 <p><?php echo $this->action("{$this->controller}/add", 'ACTION_ADD'); ?>

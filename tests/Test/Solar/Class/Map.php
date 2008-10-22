e8a27f644c99dbff1d31f19c8a7f1b2faa1f3c28
Paul M. Jones <pmjones@solarphp.com>
2008-10-22 16:03:13 +0000

    convert tests for Solar_Class_Map and Solar_Class_Stack

--- tests/Test/Solar/Class/Map.php
+++ tests/Test/Solar/Class/Map.php
@@ -90,7 +90,51 @@ class Test_Solar_Class_Map extends Solar_Test {
      */
     public function testFetch()
     {
-        $this->todo('stub');
+        $dir  = Solar_Class::dir('Solar', '..');
+        $base = realpath($dir);
+        
+        $map = Solar::factory('Solar_Class_Map');
+        $map->setBase($base);
+        
+        $actual = $map->fetch('Solar_Example');
+        $expect = array(
+            "Solar_Example"                                         => "$base/Solar/Example.php",
+            "Solar_Example_Controller_Page"                         => "$base/Solar/Example/Controller/Page.php",
+            "Solar_Example_Exception"                               => "$base/Solar/Example/Exception.php",
+            "Solar_Example_Exception_CustomCondition"               => "$base/Solar/Example/Exception/CustomCondition.php",
+            "Solar_Example_Model_Areas"                             => "$base/Solar/Example/Model/Areas.php",
+            "Solar_Example_Model_Areas_Collection"                  => "$base/Solar/Example/Model/Areas/Collection.php",
+            "Solar_Example_Model_Areas_Record"                      => "$base/Solar/Example/Model/Areas/Record.php",
+            "Solar_Example_Model_Metas"                             => "$base/Solar/Example/Model/Metas.php",
+            "Solar_Example_Model_Metas_Collection"                  => "$base/Solar/Example/Model/Metas/Collection.php",
+            "Solar_Example_Model_Metas_Record"                      => "$base/Solar/Example/Model/Metas/Record.php",
+            "Solar_Example_Model_Nodes"                             => "$base/Solar/Example/Model/Nodes.php",
+            "Solar_Example_Model_Nodes_Collection"                  => "$base/Solar/Example/Model/Nodes/Collection.php",
+            "Solar_Example_Model_Nodes_Record"                      => "$base/Solar/Example/Model/Nodes/Record.php",
+            "Solar_Example_Model_Taggings"                          => "$base/Solar/Example/Model/Taggings.php",
+            "Solar_Example_Model_Taggings_Collection"               => "$base/Solar/Example/Model/Taggings/Collection.php",
+            "Solar_Example_Model_Taggings_Record"                   => "$base/Solar/Example/Model/Taggings/Record.php",
+            "Solar_Example_Model_Tags"                              => "$base/Solar/Example/Model/Tags.php",
+            "Solar_Example_Model_Tags_Collection"                   => "$base/Solar/Example/Model/Tags/Collection.php",
+            "Solar_Example_Model_Tags_Record"                       => "$base/Solar/Example/Model/Tags/Record.php",
+            "Solar_Example_Model_TestSolarDib"                      => "$base/Solar/Example/Model/TestSolarDib.php",
+            "Solar_Example_Model_TestSolarFoo"                      => "$base/Solar/Example/Model/TestSolarFoo.php",
+            "Solar_Example_Model_TestSolarFoo_Bar"                  => "$base/Solar/Example/Model/TestSolarFoo/Bar.php",
+            "Solar_Example_Model_TestSolarFoo_Bar_Record"           => "$base/Solar/Example/Model/TestSolarFoo/Bar/Record.php",
+            "Solar_Example_Model_TestSolarFoo_Collection"           => "$base/Solar/Example/Model/TestSolarFoo/Collection.php",
+            "Solar_Example_Model_TestSolarFoo_Record"               => "$base/Solar/Example/Model/TestSolarFoo/Record.php",
+            "Solar_Example_Model_TestSolarSpecialCols"              => "$base/Solar/Example/Model/TestSolarSpecialCols.php",
+            "Solar_Example_Model_TestSolarSpecialCols_Collection"   => "$base/Solar/Example/Model/TestSolarSpecialCols/Collection.php",
+            "Solar_Example_Model_TestSolarSpecialCols_Record"       => "$base/Solar/Example/Model/TestSolarSpecialCols/Record.php",
+            "Solar_Example_Model_TestSolarSqlDescribe"              => "$base/Solar/Example/Model/TestSolarSqlDescribe.php",
+            "Solar_Example_Model_TestSolarSqlDescribe_Collection"   => "$base/Solar/Example/Model/TestSolarSqlDescribe/Collection.php",
+            "Solar_Example_Model_TestSolarSqlDescribe_Record"       => "$base/Solar/Example/Model/TestSolarSqlDescribe/Record.php",
+            "Solar_Example_Model_Users"                             => "$base/Solar/Example/Model/Users.php",
+            "Solar_Example_Model_Users_Collection"                  => "$base/Solar/Example/Model/Users/Collection.php",
+            "Solar_Example_Model_Users_Record"                      => "$base/Solar/Example/Model/Users/Record.php",
+        );
+        
+        $this->assertSame($actual, $expect);
     }
     
     /**
@@ -100,7 +144,14 @@ class Test_Solar_Class_Map extends Solar_Test {
      */
     public function testGetBase()
     {
-        $this->todo('stub');
+        $dir  = Solar_Class::dir('Solar', '..');
+        $base = Solar_Dir::fix(realpath($dir));
+        
+        $map = Solar::factory('Solar_Class_Map');
+        $map->setBase($base);
+        
+        $actual = $map->getBase();
+        $this->assertSame($actual, $base);
     }
     
     /**
@@ -110,6 +161,13 @@ class Test_Solar_Class_Map extends Solar_Test {
      */
     public function testSetBase()
     {
-        $this->todo('stub');
+        $dir  = Solar_Class::dir('Solar', '..');
+        $base = Solar_Dir::fix(realpath($dir));
+        
+        $map = Solar::factory('Solar_Class_Map');
+        $map->setBase($base);
+        
+        $actual = $map->getBase();
+        $this->assertSame($actual, $base);
     }
 }
--- tests/Test/Solar/Class/Stack.php
+++ tests/Test/Solar/Class/Stack.php
@@ -90,7 +90,47 @@ class Test_Solar_Class_Stack extends Solar_Test {
      */
     public function testAdd()
     {
-        $this->todo('stub');
+        $stack = Solar::factory('Solar_Class_Stack');
+        $stack->add('Base_Foo');
+        $stack->add('Base_Bar');
+        $stack->add('Base_Baz');
+        
+        $expect = array(
+          "Base_Baz_",
+          "Base_Bar_",
+          "Base_Foo_",
+        );
+        
+        $this->assertSame($stack->get(), $expect);
+    }
+    
+    public function testAdd_array()
+    {
+        $stack = Solar::factory('Solar_Class_Stack');
+        $stack->add(array('Base_Foo', 'Base_Bar', 'Base_Baz'));
+        
+        $expect = array(
+          "Base_Foo_",
+          "Base_Bar_",
+          "Base_Baz_",
+        );
+        
+        $this->assertSame($stack->get(), $expect);
+    }
+    
+    public function testAdd_string()
+    {
+        // add to the stack as a csv list
+        $stack = Solar::factory('Solar_Class_Stack');
+        $stack->add('Base_Foo, Base_Bar, Base_Baz');
+        
+        $expect = array(
+          "Base_Foo_",
+          "Base_Bar_",
+          "Base_Baz_",
+        );
+        
+        $this->assertSame($stack->get(), $expect);
     }
     
     /**
@@ -100,7 +140,15 @@ class Test_Solar_Class_Stack extends Solar_Test {
      */
     public function testGet()
     {
-        $this->todo('stub');
+        $expect = array(
+          'Base_Foo_',
+          'Base_Bar_',
+          'Base_Baz_',
+        );
+        
+        $stack = Solar::factory('Solar_Class_Stack');
+        $stack->set('Base_Foo, Base_Bar, Base_Baz');
+        $this->assertSame($stack->get(), $expect);
     }
     
     /**
@@ -110,7 +158,27 @@ class Test_Solar_Class_Stack extends Solar_Test {
      */
     public function testLoad()
     {
-        $this->todo('stub');
+        // try loading 'Stack' from Solar_Path and Solar_Class:
+        // should find it first at Solar_Path_Stack.
+        $stack = Solar::factory('Solar_Class_Stack');
+        $stack->set('Solar_Path, Solar_Class');
+        $actual = $stack->load('Stack');
+        $expect = 'Solar_Path_Stack';
+        $this->assertSame($actual, $expect);
+        
+        // now try loading 'Map': should not find it at Solar_Path, so
+        // should fall back to Solar_Class.
+        $actual = $stack->load('Map');
+        $expect = 'Solar_Class_Map';
+        $this->assertSame($actual, $expect);
+        
+        // should fail to find this anywhere on the stack
+        try {
+            $stack->load('NoSuchClass');
+            $this->fail('Should have thrown an exception here');
+        } catch (Solar_Exception $e) {
+            // do nothing
+        }
     }
     
     /**
@@ -120,6 +188,29 @@ class Test_Solar_Class_Stack extends Solar_Test {
      */
     public function testSet()
     {
-        $this->todo('stub');
+        $expect = array(
+          'Base_Foo_',
+          'Base_Bar_',
+          'Base_Baz_',
+        );
+        
+        $stack = Solar::factory('Solar_Class_Stack');
+        $stack->set($expect);
+        
+        $this->assertSame($stack->get(), $expect);
+    }
+    
+    public function testSet_string()
+    {
+        $expect = array(
+          'Base_Foo_',
+          'Base_Bar_',
+          'Base_Baz_',
+        );
+        
+        $stack = Solar::factory('Solar_Class_Stack');
+        $stack->set('Base_Foo, Base_Bar, Base_Baz');
+        
+        $this->assertSame($stack->get(), $expect);
     }
 }

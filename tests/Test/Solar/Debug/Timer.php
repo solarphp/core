980db49a5e9a06893eacc52c2e01af3264da915e
Paul M. Jones <pmjones@solarphp.com>
2008-10-28 18:56:51 +0000

    tests: add Solar_Debug_Timer tests, remove old phpunit ones

--- tests/Test/Solar/Debug/Timer.php
+++ tests/Test/Solar/Debug/Timer.php
@@ -16,6 +16,8 @@ class Test_Solar_Debug_Timer extends Solar_Test {
     protected $_Test_Solar_Debug_Timer = array(
     );
     
+    protected $_delta = 0.005;
+    
     // -----------------------------------------------------------------
     // 
     // Support methods.
@@ -110,7 +112,58 @@ class Test_Solar_Debug_Timer extends Solar_Test {
      */
     public function testMark()
     {
-        $this->todo('stub');
+        $timer = Solar::factory('Solar_Debug_Timer');
+        
+        $expect[0]['time'] = microtime(true);
+        $timer->start();
+        
+        $uwait = rand(3, 8) * 100000;
+        usleep($uwait);
+        
+        $expect[1]['time'] = microtime(true);
+        $timer->mark('foo');
+        
+        $actual = $timer->profile();
+        
+        $count = count($expect);
+        $this->assertSame(count($actual), $count);
+        
+        $keys = array('name', 'time', 'diff', 'total');
+        $this->assertSame(array_keys($actual[1]), $keys);
+        
+        $this->assertSame($actual[1]['name'], 'foo');
+        
+        $this->assertInDelta(
+            $actual[1]['time'],
+            $expect[1]['time'],
+            $this->_delta
+        );
+        
+        $expect[1]['diff'] = $expect[1]['time'] - $expect[0]['time'];
+        $this->assertInDelta(
+            $actual[1]['diff'],
+            $expect[1]['diff'],
+            $this->_delta
+        );
+        
+        $expect[1]['total'] = $expect[1]['time'] - $expect[0]['time'];
+        $this->assertInDelta(
+            $actual[1]['diff'],
+            $expect[1]['diff'],
+            $this->_delta
+        );
+        
+        // do another mark to test the total time
+        $expect[2]['time'] = microtime(true);
+        $timer->mark('bar');
+        $actual = $timer->profile();
+        
+        $expect[2]['total'] = $expect[2]['time'] - $expect[0]['time'];
+        $this->assertInDelta(
+            $actual[2]['total'],
+            $expect[2]['total'],
+            $this->_delta
+        );
     }
     
     /**
@@ -120,7 +173,31 @@ class Test_Solar_Debug_Timer extends Solar_Test {
      */
     public function testProfile()
     {
-        $this->todo('stub');
+        $timer = Solar::factory('Solar_Debug_Timer');
+        
+        $expect[0]['time'] = microtime(true);
+        $timer->start();
+        
+        $k = rand(3, 8);
+        for ($i = 1; $i <= $k; $i++) {
+            $uwait = rand(3, 8) * 100000;
+            usleep($uwait);
+            $expect[$i]['time'] = microtime(true);
+            $timer->mark($i);
+        }
+        
+        $k++;
+        $expect[$k]['time'] = microtime(true);
+        $timer->stop();
+        
+        $actual = $timer->profile();
+        for ($i = 0; $i <= $k; $i ++) {
+            $this->assertInDelta(
+                $actual[$i]['time'],
+                $expect[$i]['time'],
+                $this->_delta
+            );
+        }
     }
     
     /**
@@ -130,7 +207,18 @@ class Test_Solar_Debug_Timer extends Solar_Test {
      */
     public function testStart()
     {
-        $this->todo('stub');
+        $timer = Solar::factory('Solar_Debug_Timer');
+        
+        $expect[0]['time'] = microtime(true);
+        $timer->start();
+        
+        $actual = $timer->profile();
+        
+        $this->assertInDelta(
+            $actual[0]['time'],
+            $expect[0]['time'],
+            $this->_delta
+        );
     }
     
     /**
@@ -140,6 +228,59 @@ class Test_Solar_Debug_Timer extends Solar_Test {
      */
     public function testStop()
     {
-        $this->todo('stub');
+        $timer = Solar::factory('Solar_Debug_Timer');
+        
+        $expect[0]['time'] = microtime(true);
+        $timer->start();
+        
+        $uwait = rand(3, 8) * 100000;
+        usleep($uwait);
+        
+        $expect[1]['time'] = microtime(true);
+        $timer->stop();
+        
+        $actual = $timer->profile();
+        $this->assertInDelta(
+            $actual[1]['time'],
+            $expect[1]['time'],
+            $this->_delta
+        );
+    }
+    
+    // public function testAll()
+    // {
+    //     // does the class create the locale config?
+    //     $timer = Solar::factory('Solar_Debug_Timer', array('output' => 'text'));
+    //     
+    //     $mark['__start'] = microtime(true);
+    //     $timer->start();
+    //     for ($i = 0; $i < 4; $i++) {
+    //         $wait = rand(1,2);
+    //         sleep($wait);
+    //         $mark[$i] = microtime(true);
+    //         $timer->mark($i);
+    //     }
+    //     $mark['__stop'] = microtime(true);
+    //     $timer->stop();
+    //     
+    //     // get the timer profile
+    //     $profile = $timer->profile();
+    //     
+    //     // make sure we hit all the marks
+    //     $this->assertTrue(count($profile) == count($mark));
+    //     
+    //     foreach ($profile as $val) {
+    //         // make sure the profiled times are near the
+    //         // times we marked
+    //         $key = $val['name'];
+    //         $diff = abs($val['time'] - $mark[$key]);
+    //         $this->assertTrue($diff <= 0.0005);
+    //     }
+    // }
+    
+    public function assertInDelta($actual, $expect, $delta)
+    {
+        $diff = abs($actual - $expect);
+        $this->assertTrue($diff <= $delta);
     }
 }

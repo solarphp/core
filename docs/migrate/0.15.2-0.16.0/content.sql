803943c139f076f81746130bbdf8a93d19aca952
Paul M. Jones <pmjones@solarphp.com>
2009-09-20 13:52:15 +0000

    removing way-old migration docs

--- docs/migrate/0.15.2-0.16.0/content.sql
+++
@@ -1,6 +0,0 @@
-# 'position' becomes 'pos'
-ALTER TABLE nodes ADD COLUMN pos INTEGER;
-CREATE INDEX nodes__pos__i ON nodes (pos);
-UPDATE nodes SET pos = position;
-DROP INDEX nodes__position__i ON nodes;
-ALTER TABLE nodes DROP COLUMN position;
--- docs/migrate/1.0.0alpha1-1.0.0alpha2/nodes.sql
+++
@@ -1,3 +0,0 @@
-# this is for MySQL -- other users will need to translate.
-ALTER TABLE `nodes` ADD COLUMN `publish` DATETIME;
-CREATE INDEX `publish` ON `nodes` (`publish`);

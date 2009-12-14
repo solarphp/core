51a28203b5db6a0fb6d1ce7a51abeeb610f015e6
Paul M. Jones <pmjones@solarphp.com>
2009-12-14 02:54:59 +0000

    Solar_Cli_MakeModel: [CHG] Put setup file reads in _preSetup(), not _setup().

--- Solar/Cli/MakeModel/Data/model.php
+++ Solar/Cli/MakeModel/Data/model.php
@@ -7,6 +7,25 @@ class {:class} extends {:extends}
 {
     /**
      * 
+     * Establish state of this object prior to _setup().
+     * 
+     * @return void
+     * 
+     */
+    protected function _preSetup()
+    {
+        // chain to parent
+        parent::_preSetup();
+        
+        // use setup files generated from make-model
+        $setup_dir         = Solar_Class::dir(__CLASS__, 'Setup');
+        $this->_table_name = Solar_File::load("{$setup_dir}/table_name.php");
+        $this->_table_cols = Solar_File::load("{$setup_dir}/table_cols.php");
+        $this->_index      = Solar_File::load("{$setup_dir}/index_info.php");
+    }
+    
+    /**
+     * 
      * Model-specific setup.
      * 
      * @return void
@@ -14,9 +33,7 @@ class {:class} extends {:extends}
      */
     protected function _setup()
     {
-        $setup             = Solar_Class::dir(__CLASS__, 'Setup');
-        $this->_table_name = Solar_File::load($setup . 'table_name.php');
-        $this->_table_cols = Solar_File::load($setup . 'table_cols.php');
-        $this->_index      = Solar_File::load($setup . 'index_info.php');
+        // chain to parent
+        parent::_setup();
     }
 }

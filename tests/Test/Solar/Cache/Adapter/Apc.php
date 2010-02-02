16f02e4eb4e922e741fdb47939daa12b30bc1168
Paul M. Jones <pmjones@solarphp.com>
2010-02-02 00:26:24 +0000

    skip testing when apc is not enabled

--- tests/Test/Solar/Cache/Adapter/Apc.php
+++ tests/Test/Solar/Cache/Adapter/Apc.php
@@ -22,4 +22,12 @@ class Test_Solar_Cache_Adapter_Apc extends Test_Solar_Cache_Adapter {
     );
     
     protected $_extension = 'apc';
+    
+    public function __construct($config = null)
+    {
+        parent::__construct($config);
+        if (PHP_SAPI == 'cli' && ! ini_get('apc.enable_cli')) {
+            $this->skip('apc loaded but not enabled for cli');
+        }
+    }
 }

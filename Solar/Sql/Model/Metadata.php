192efc1264faf616202167a352c9b7672c29bde5
Paul M. Jones <pmjones@solarphp.com>
2010-02-05 16:51:41 +0000

    [ADD] Class Solar_Sql_Model_Metadata to take the place of Setup/table_name, table_cols, and index_info files

---
+++ Solar/Sql/Model/Metadata.php
@@ -0,0 +1,9 @@
+<?php
+abstract class Solar_Sql_Model_Metadata extends Solar_Base
+{
+    public $table_name = null;
+    
+    public $table_cols = array();
+    
+    public $index_info = array();
+}

8c0c73123f77dab8bb0bc755da7c06d591301a2a
Paul M. Jones <pmjones@solarphp.com>
2010-02-17 16:09:18 +0000

    Solar_View_Helper_FormCheckbox: [ADD] Add config key 'label_class' to add a CSS class to the label tag wrapping the checkbox input; this will help target labels on checkboxes (vs generic input labels).

--- Solar/View/Helper/FormCheckbox.php
+++ Solar/View/Helper/FormCheckbox.php
@@ -16,6 +16,10 @@
  */
 class Solar_View_Helper_FormCheckbox extends Solar_View_Helper_FormElement
 {
+    protected $_Solar_View_Helper_FormCheckbox = array(
+        'label_class' => 'checkbox',
+    );
+    
     /**
      * 
      * Generates a 'checkbox' element.
@@ -55,7 +59,16 @@ class Solar_View_Helper_FormCheckbox extends Solar_View_Helper_FormElement
                
         // wrap in a label?
         if ($this->_label) {
-            $xhtml = '<label>' . $xhtml . $this->_view->escape($this->_label) . '</label>';
+            if ($this->_config['label_class']) {
+                $attribs = $this->_view->attribs(array(
+                    'class' => $this->_config['label_class'],
+                ));
+            } else {
+                $attribs = null;
+            }
+            
+            $label = $this->_view->escape($this->_label);
+            $xhtml = "<label{$attribs}>{$xhtml} {$label}</label>";
         }
         
         // prefix with unchecked value

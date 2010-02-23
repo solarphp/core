b102c06f71fe4269678666ef951883c159ee8820
Paul M. Jones <pmjones@solarphp.com>
2010-02-23 20:56:31 +0000

    add new exception classes

---
+++ Solar/Exception/DirNotFound.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 
+ * Exception: directory was not found.
+ * 
+ * @category Solar
+ * 
+ * @package Solar
+ * 
+ * @author Paul M. Jones <pmjones@solarphp.com>
+ * 
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ * 
+ * @version $Id: ConnectionFailed.php 2933 2007-11-09 20:37:35Z moraes $
+ * 
+ */
+class Solar_Exception_DirNotFound extends Solar_Exception {}
---
+++ Solar/Exception/RmdirFailed.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 
+ * Exception: failed to remove a directory.
+ * 
+ * @category Solar
+ * 
+ * @package Solar
+ * 
+ * @author Paul M. Jones <pmjones@solarphp.com>
+ * 
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ * 
+ * @version $Id: ConnectionFailed.php 2933 2007-11-09 20:37:35Z moraes $
+ * 
+ */
+class Solar_Exception_RmdirFailed extends Solar_Exception {}

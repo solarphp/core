7fadbb5438dbdfe64d862d946cc3aa5d7c79cb3b
Paul M. Jones <pmjones@solarphp.com>
2010-02-23 20:57:06 +0000

    add new exception class

---
+++ Solar/Sql/Adapter/Exception/NoColsFound.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 
+ * Exception: no columns found for a table.
+ * 
+ * @category Solar
+ * 
+ * @package Solar_Sql
+ * 
+ * @author Paul M. Jones <pmjones@solarphp.com>
+ * 
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ * 
+ * @version $Id: TableNotCreated.php 2933 2007-11-09 20:37:35Z moraes $
+ * 
+ */
+class Solar_Sql_Adapter_Exception_NoColsFound extends Solar_Sql_Adapter_Exception {}

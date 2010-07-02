ecf26c7b4846d3a2d23b55ff3aaf620660f81335
Paul M. Jones <pmjones@solarphp.com>
2010-07-02 14:25:57 +0000

    rename helper FacebookLink to FacebookLoginButton: [CHG] Support all login button attribs.

--- Solar/View/Helper/FacebookLoginButton.php
+++ Solar/View/Helper/FacebookLoginButton.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * 
- * Generates the Link for a facebook login
+ * Generates a Facebook login button.
  * 
  * @category Solar
  * 
@@ -16,27 +16,77 @@
  * @version $Id: TypekeyLink.php 4285 2009-12-31 02:18:15Z pmjones $
  * 
  */
-class Solar_View_Helper_FacebookLink extends Solar_View_Helper
+class Solar_View_Helper_FacebookLoginButton extends Solar_View_Helper
 {
-    protected $_Solar_View_Helper_FacebookScript = array(
-        'perms'             => 'email',
-        'size'              => 'medium',
-        'autologoutlink'    => 'false',
+    /**
+     * 
+     * Default configuration values. See the original documentation at
+     * <http://wiki.developers.facebook.com/index.php/Fb:login-button>.
+     * 
+     * @config string condition Indicates whether the button is visible or 
+     * hidden. (See the wiki for more information.)
+     * 
+     * @config string size Specifies the size of the button. Specify 'icon' to 
+     * display a favicon only, or 'small', 'medium', 'large', or 'xlarge'.
+     * 
+     * @config string autologoutlink If 'true' and the user is already 
+     * connected and has a session, then the button image changes to 
+     * indicate the user can log out. Clicking the button logs the user 
+     * out of Facebook and all connected sessions. Note that 'true'/'false'
+     * values must be strings, not PHP booleans.
+     * 
+     * @config string background Specifies the button image to use that is 
+     * anti-aliased to match the background of your site -- whether it's 
+     * pure white, light, or dark. Specify 'white', 'dark', or 'light'. 
+     * Note: You don't specify this attribute if you are using v="2".  
+     * 
+     * @config string length Specifies which text label to use on a button 
+     * with size specified as 'small', 'medium', 'large', or 'xlarge'. 
+     * Specify 'short' for the text label **Connect** only or 'long' for 
+     * the text label **Connect with Facebook**. If you are rendering the 
+     * login button text by including it within the fb:login-button tags, 
+     * you don't specify a length at all.
+     * 
+     * @config string onlogin JavaScript code to execute when the user gains 
+     * a Facebook session (that is, after logging into Facebook and 
+     * authorizing the site).
+     * 
+     * @config string v Specify "2" to use the latest Facebook Connect 
+     * login buttons (examples available in the Facebook Connect wizard). 
+     * Don't use the attribute if you need to use the original Facebook 
+     * Connect login buttons.
+     * 
+     * * @var array
+     * 
+     */
+    protected $_Solar_View_Helper_FacebookLoginButton = array(
+        'condition'         => null,
+        'size'              => null,
+        'autologoutlink'    => null,
+        'background'        => null,
+        'length'            => null,
+        'onlogin'           => null,
+        'perm'              => 'email',
     );
 
     /**
      * 
-     * Generates the link required by facebook
+     * Generates a Facebook login button.
      * 
      * @return string
      * 
      */
-    public function FacebookLink()
+    public function facebookLoginButton()
     {
-        return '<fb:login-button autologoutlink="'.$this->_config['autologoutlink'].
-                '" size="'.$this->_config['size'].
-                '" perm="'.$this->_config['perms'].
-                '"></fb:login-button>';
+        $attribs = array();
+        foreach ($this->_config as $key => $val) {
+            if ($val !== null) {
+                $attribs[$key] = $val;
+            }
+        }
+        
+        return '<fb:login-button'
+             . $this->_view->attribs($attribs)
+             . '></fb:login-button>';
     }
-
 }

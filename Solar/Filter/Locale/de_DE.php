3f9efb0ecbe3572132a98159e72145538800f715
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2010-12-14 01:01:19 +0100

    Encoded to utf8

--- Solar/Filter/Locale/de_DE.php
+++ Solar/Filter/Locale/de_DE.php
@@ -1,96 +1,55 @@
 <?php
 return array(
+
+
     'INVALID_ALNUM'                     => 'Bitte nur Buchstaben (A-Z und a-z) und Ziffern (0-9) eingeben.',
-
     'INVALID_ALPHA'                     => 'Bitte nur Buchstaben (A-Z und a-z) eingeben.',
-
-    'INVALID_BLANK'                     => 'Die Eingabe muß leer sein.',
-
+    'INVALID_BLANK'                     => 'Die Eingabe muÃŸ leer sein.',
     'INVALID_BOOL'                      => 'Die Eingabe sollte true (wahr, ja) oder false (falsch, nein) sein.',
-
-    'INVALID_COMPARE'                   => 'Die Eingabe stimmt nicht mit der anderen überein',
-
-    'INVALID_CONFIRM'                   => 'Die Eingabe stimmt nicht mit der anderen überein',
-
+    'INVALID_COMPARE'                   => 'Die Eingabe stimmt nicht mit der anderen Ã¼berein',
+    'INVALID_CONFIRM'                   => 'Die Eingabe stimmt nicht mit der anderen Ã¼berein',
     'INVALID_CTYPE'                     => 'Die Eingabe entspricht nicht dem notwendigen C-Typ.',
-
-    'INVALID_EMAIL'                     => 'Bitte gültige Email Adresse eingeben..',
-
+    'INVALID_EMAIL'                     => 'Bitte gÃ¼ltige Email Adresse eingeben..',
     'INVALID_FLOAT'                     => 'Bitte Gleitkommazahl eingeben.',
-
     'INVALID_INT'                       => 'Bitte nur ganze Zahlen eingeben.',
-
-    'INVALID_IN_KEYS'                   => 'Bitte wähle einen anderen Wert.',
-
-    'INVALID_IN_LIST'                   => 'Bitte wähle einen anderen Wert.',
-
-    'INVALID_IP'                        => 'Bitte gültige IP Adresse (v4 oder v6) eingeben.',
-
-    'INVALID_IPV4'                      => 'Bitte gültige IPv4 eingeben.',
-
-    'INVALID_IPV6'                      => 'Bitte gültige IPv6 eingeben.',
-
+    'INVALID_IN_KEYS'                   => 'Bitte wÃ¤hle einen anderen Wert.',
+    'INVALID_IN_LIST'                   => 'Bitte wÃ¤hle einen anderen Wert.',
+    'INVALID_IP'                        => 'Bitte gÃ¼ltige IP Adresse (v4 oder v6) eingeben.',
+    'INVALID_IPV4'                      => 'Bitte gÃ¼ltige IPv4 eingeben.',
+    'INVALID_IPV6'                      => 'Bitte gÃ¼ltige IPv6 eingeben.',
     'INVALID_ISO_DATE'                  => 'Bitte gib ein Datum im "JJJJ-MM-TT" Format ein.',
 
+
     'INVALID_ISO_TIME'                  => 'Bitte gib eine Zeit im "HH:MM:SS" Format ein.',
-
     'INVALID_ISO_TIMESTAMP'             => 'Bitte gib einen Zeitstempel im "JJJJ-MM-TTThh:HH:SS" Format ein.',
-
-    'INVALID_LOCALE_CODE'               => 'Bitte gib einen gültigen Sprachcode im ISO 639 Format ein (z.B. de_DE).',
-
+    'INVALID_LOCALE_CODE'               => 'Bitte gib einen gÃ¼ltigen Sprachcode im ISO 639 Format ein (z.B. de_DE).',
     'INVALID_MAX'                       => 'Bitte gib einen kleineren Wert ein.',
-
-    'INVALID_MAX_LENGTH'                => 'Bitte gib eine kürzere Zeichenkette ein.',
-
-    'INVALID_MIME_TYPE'                 => 'Bitte gib einen gültigen mime-type (z.B. "text/plain") ein.',
-
-    'INVALID_MIN'                       => 'Bitte gib einen größeren Wert ein.',
-
-    'INVALID_MIN_LENGTH'                => 'Bitte gib eine längere Zeichenkette ein..',
-
+    'INVALID_MAX_LENGTH'                => 'Bitte gib eine kÃ¼rzere Zeichenkette ein.',
+    'INVALID_MIME_TYPE'                 => 'Bitte gib einen gÃ¼ltigen mime-type (z.B. "text/plain") ein.',
+    'INVALID_MIN'                       => 'Bitte gib einen grÃ¶ÃŸeren Wert ein.',
+    'INVALID_MIN_LENGTH'                => 'Bitte gib eine lÃ¤ngere Zeichenkette ein..',
     'INVALID_NOT_BLANK'                 => 'Die Eingabe darf nicht leer sein.',
-
     'INVALID_NOT_ZERO'                  => 'Die Eingabe darf nicht Null sein.',
-
     'INVALID_NUMERIC'                   => 'Bitte gib eine Zahl ein.',
-
-    'INVALID_PREG_MATCH'                => 'Die Eingabe entspricht nicht dem regulärem Ausdruck.',
-
+    'INVALID_PREG_MATCH'                => 'Die Eingabe entspricht nicht dem regulÃ¤rem Ausdruck.',
     'INVALID_RANGE'                     => 'Die Eingabe liegt ausserhalb des erlaubten Wertebereichs',
-
     'INVALID_RANGE_LENGTH'              => 'Die Eingabe ist zu kurz oder zu lang.',
-
     'INVALID_SEP_WORDS'                 => 'Bitte nur Buchstaben (A-Z und a-z), Ziffern (0-9), Unterstriche(_), und Leerzeichen eingeben.',
 
     'INVALID_STRING'                    => 'Bitte gib eine Zeichenkette ein.',
-
     'INVALID_UNIQUE'                    => 'Die Eingabe wird schon verwendet.',
-
     'INVALID_UPLOAD_ARRAY_MALFORMED'    => 'Der Upload Informations Array ist korrumpiert.',
-
     'INVALID_UPLOAD_CANT_WRITE'         => 'Fehler beim Schreiben der hochgeladenen Datei auf die Festplatte.',
-
     'INVALID_UPLOAD_EXTENSION'          => 'Der Hochladevorgang wurde von einer PHP Erweiterung unterbrochen.',
-
-    'INVALID_UPLOAD_FILENAME_EXT'       => 'Bitte Datei mit anderer Dateinamenendung auswählen.',
-
-    'INVALID_UPLOAD_FORM_SIZE'          => 'Die hochgeladene Datei ist größer als MAX_FILE_SIZE Angabe im HTML Formular.',
-
-    'INVALID_UPLOAD_INI_SIZE'           => 'Die hochgeladene Datei ist größer als upload_max_filesize angabe in der php.ini.',
-
+    'INVALID_UPLOAD_FILENAME_EXT'       => 'Bitte Datei mit anderer Dateinamenendung auswÃ¤hlen.',
+    'INVALID_UPLOAD_FORM_SIZE'          => 'Die hochgeladene Datei ist grÃ¶ÃŸer als MAX_FILE_SIZE Angabe im HTML Formular.',
+    'INVALID_UPLOAD_INI_SIZE'           => 'Die hochgeladene Datei ist grÃ¶ÃŸer als upload_max_filesize angabe in der php.ini.',
     'INVALID_UPLOAD_NOT_ARRAY'          => 'Die Upload Information ist kein Array.',
-
     'INVALID_UPLOAD_NOT_UPLOADED_FILE'  => 'PHP meldet das die Datei "kein hochgeladene Datei" ist.',
-
     'INVALID_UPLOAD_NO_FILE'            => 'Keine Datei hochgeladen.',
-
-    'INVALID_UPLOAD_NO_TMP_DIR'         => 'Das temporäre Verzeichnis für hochgeladene Dateien fehlt.',
-
+    'INVALID_UPLOAD_NO_TMP_DIR'         => 'Das temporÃ¤re Verzeichnis fÃ¼r hochgeladene Dateien fehlt.',
     'INVALID_UPLOAD_PARTIAL'            => 'Die Datei wurde nur teilweise hochgeladen.',
-
     'INVALID_UPLOAD_UNKNOWN_ERROR'      => 'Ein unbekannter Fehler ist bei der Validierung der Datei aufgetreten.',
-
-    'INVALID_URI'                       => 'Bitte gültige Web-Adresse eingeben.',
-
+    'INVALID_URI'                       => 'Bitte gÃ¼ltige Web-Adresse eingeben.',
     'INVALID_WORD'                      => 'Bitte nur Buchstaben (A-Z und a-z), Ziffern (0-9) und Unterstriche (_) eingeben.',
     );
